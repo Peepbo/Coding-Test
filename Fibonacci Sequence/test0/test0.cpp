777d3c59fe1a2811229c7962edd26bf5bdb78123
Peepbo <tngus10778@gmail.com>
2020-12-01 17:28:22 +0900

    Fibonacci Sequence

---
+++ Fibonacci Sequence/test0/test0.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+
+    int before = 0;
+
+    //0, 1, 1, 2, 3, 5
+
+    for (int i = 0; i <= n; i++)
+    {
+        if (i == 1)
+        {
+            answer = i;
+            before = 0;
+        }
+
+        else
+        {
+            int save = answer;
+
+            answer += before;
+            before = save;
+
+            answer %= 1234567;
+        }
+    }
+    return answer;
+}
+
+void main()
+{
+    solution(30);
+}
