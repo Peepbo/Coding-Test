41b200f086cd11fb2fcf3f29202c7c36493706ce
Peepbo <tngus10778@gmail.com>
2020-12-02 21:59:51 +0900

    Correct Bracket

---
+++ Correct bracket/test0/test0.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+    int tmp = 0;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[0] == ')' && s[s.length()] == '(')
+        {
+            return false;
+        }
+        if (s[i] == '(')
+        {
+            tmp++;
+        }
+        else
+        {
+            tmp--;
+            if (tmp < 0)
+            {
+                return false;
+            }
+        }
+    }
+
+    if (tmp == 0) {
+        return true;
+    }
+
+    return false;
+}
+
+void main()
+{
+	cout << solution("(())()");
+}
