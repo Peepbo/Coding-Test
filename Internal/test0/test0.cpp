59880b17b0db88f654e070d5220b6430ae84ee6c
Peepbo <tngus10778@gmail.com>
2020-12-03 19:11:04 +0900

    Programers - Internal

---
+++ Internal/test0/test0.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> a, vector<int> b) {
+    int answer = 0;
+
+    while (true)
+    {
+        if (a.empty())return answer;
+
+        answer += a.back() * b.back();
+
+        a.pop_back();
+        b.pop_back();
+    }
+
+    return answer;
+}
+
+void main()
+{
+    cout << solution({ -1,0,1 }, { 1,0,-1 });
+}
