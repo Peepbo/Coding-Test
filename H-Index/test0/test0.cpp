dd2c94387ebb3e583f286989c8ade9cf5f8c1a21
Peepbo <tngus10778@gmail.com>
2020-12-04 22:33:19 +0900

    Programers - H-Index

---
+++ H-Index/test0/test0.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> citations) {
+    int answer = 0;
+
+    sort(citations.begin(), citations.end(), greater<int>());
+
+    for (int i = 0; i <= citations.size(); i++)
+    {
+        if (i < citations[i])
+        {
+            answer++;
+        }
+
+        else break;
+    }
+
+    return answer;
+}
+
+void main()
+{
+    cout << solution({ 3, 0, 6, 1, 5 });
+}
