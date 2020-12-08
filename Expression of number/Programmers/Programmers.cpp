6e08dbe26365e41c75dd1385008e2f8304b1f852
Peepbo <tngus10778@gmail.com>
2020-12-08 16:42:16 +0900

    Programmers - Expression of number

---
+++ Expression of number/Programmers/Programmers.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 1;
+    int Order = 1;
+
+    while (Order < n)
+    {
+        int num = Order;
+        int last = num;
+
+        while (num < n)
+        {
+            last++;
+
+            num += last;
+
+            if (num == n) answer++;
+        }
+        
+        Order++;
+    }
+
+    return answer;
+}
+
+int main()
+{
+    cout << solution(15);
+}
