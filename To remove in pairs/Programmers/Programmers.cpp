f46575b71a08d33082c0b85b0e807659640df592
Peepbo <tngus10778@gmail.com>
2020-12-10 17:05:42 +0900

    Programmers - To remove in pairs

---
+++ To remove in pairs/Programmers/Programmers.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include<string>
+#include<stack>
+using namespace std;
+
+int solution(string s)
+{
+    int answer = 0;
+    stack<char> st;
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (st.empty() || st.top() != s[i]) st.push(s[i]);
+        else st.pop();
+    }
+
+    if (st.empty())return 1;
+
+    return answer;
+}
+
+int main()
+{
+    cout << solution("baabaa");
+}
