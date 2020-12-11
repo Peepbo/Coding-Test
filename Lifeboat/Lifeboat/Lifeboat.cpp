553a05fdeb717d7ab07b18c11106b562c9260e83
Peepbo <tngus10778@gmail.com>
2020-12-11 15:15:36 +0900

    Programmers - Lifeboat

---
+++ Lifeboat/Lifeboat/Lifeboat.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+    int answer = 0;
+    int head = 0, tail = people.size() - 1;
+    sort(people.begin(), people.end());
+
+    while (head <= tail)
+    {
+        if (people[head] + people[tail] <= limit)
+        {
+            head++;
+            tail--;
+        }
+        else tail--;
+
+        answer++;
+    }
+
+    return answer;
+}
+
+int main()
+{
+    cout << solution({ 10,10,10,80 }, 100);
+}
