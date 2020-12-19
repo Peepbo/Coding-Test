f1cf8bd6721e7dc1392e938ba34dee630e668032
Peepbo <tngus10778@gmail.com>
2020-12-19 21:51:53 +0900

    Baekjoon - I'm Happy~

---
+++ I'm Happy~/baekjoon/baekjoon.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int N, M, K;
+	cin >> N >> M >> K;
+
+	int count = 0;
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			if (count == K)
+			{
+				cout << i << " " << j;
+				return 0;
+			}
+
+			count++;
+		}
+	}
+}
