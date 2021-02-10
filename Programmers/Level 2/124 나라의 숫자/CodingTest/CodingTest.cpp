8e64fdc83c08e84d0f4c9b480d078b08a1d854b0
Peepbo <tngus10778@gmail.com>
2021-02-10 19:41:04 +0900

    Programmers - 124 나라의 숫자

---
+++ Programmers/Level 2/124 나라의 숫자/CodingTest/CodingTest.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+string solution(int n) {
+	string answer = "";
+	stack<char> stk;
+	char ch, topNum;
+
+	while (n > 0)
+	{
+		if (n % 3 == 0)
+		{
+			ch = 4 + '0';
+			n = n / 3 - 1;
+		}
+		else
+		{
+			ch = n % 3 + '0';
+			n /= 3;
+		}
+		stk.push(ch);
+	}
+
+	while (!stk.empty())
+	{
+		topNum = stk.top();
+		stk.pop();
+		answer += topNum;
+	}
+
+	return answer;
+}
+
+int main()
+{
+	cout << solution(3);
+
+	return 0;
+}
