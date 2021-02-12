17ef08a7ecd59819a4636c1b1792fb4e8dad73a8
Peepbo <tngus10778@gmail.com>
2021-02-12 17:36:52 +0900

    Programmers - 기능개발

---
+++ Programmers/Level 2/기능개발/CodingTest/CodingTest.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+	vector<int> answer;
+	vector<int> days;
+
+	int len = progresses.size();
+	int persent, remains;
+
+	for (int i = 0; i < len; i++)
+	{
+		persent = 100 - progresses[i];
+		remains = persent / speeds[i];
+
+		if (persent % speeds[i] == 0)
+			days.emplace_back(remains);
+		else
+			days.emplace_back(remains + 1);
+	}
+
+	int now = days[0];
+	answer.emplace_back(1);
+
+	for (int i = 1; i < len; i++)
+	{
+		if (now < days[i])
+		{
+			answer.emplace_back(1);
+			now = days[i];
+		}
+		else answer.back()++;
+	}
+
+	return answer;
+}
+
+int main()
+{
+	vector<int> v = solution({ 95,90,99,99,80,99 }, { 1,1,1,1,1,1 });
+	//for (int i = 0; i < v.size(); i++) cout << v[i] << '\n';
+
+	return 0;
+}
