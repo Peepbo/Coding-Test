4dc39dff1b9dd5dd0c24d0f83aa122b67f2b41c2
Peepbo <tngus10778@gmail.com>
2021-02-14 18:24:43 +0900

    Programmers - 소수 만들기

---
+++ Programmers/Level 2/소수 만들기/CodingTest/CodingTest.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+#define SIZE 3150
+
+int arr[SIZE];
+int solution(vector<int> nums) {
+    int answer = 0;
+
+    arr[0] = arr[1] = 0;
+    for (int i = 2; i < SIZE; i++) arr[i] = i;
+
+    for (int i = 2; i < SIZE; i++)
+    {
+        if (arr[i] == 0)continue;
+
+        for (int j = i * 2; j < SIZE; j += i)
+        {
+            arr[j] = 0;
+        }
+    }
+
+    int len = nums.size();
+    for (int i = 0; i < len; i++)
+    {
+        for (int j = i + 1; j < len; j++)
+        {
+            for (int k = j + 1; k < len; k++)
+            {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (arr[sum] != 0) answer++;
+            }
+        }
+    }
+
+    return answer;
+}
+
+int main()
+{
+    cout << solution({1,2,7,6,4});
+
+	return 0;
+}
