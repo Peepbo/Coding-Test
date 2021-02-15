2115a26bae39a2a50c0045aa704e0675631225f2
Peepbo <tngus10778@gmail.com>
2021-02-15 15:56:38 +0900

    Programmers - 큰 수 만들기

---
+++ Programmers/Level 2/큰 수 만들기/CodingTest/CodingTest.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string number, int k) {
+    string answer = "";
+    int len = number.size();
+
+    int pivot = -1;
+    for (int i = 0; i < len - k; i++)
+    {
+        char max = 0;
+
+        for (int j = pivot + 1; j <= k + i; j++)
+        {
+            if (max < number[j])
+            {
+                max = number[j];
+                pivot = j;
+            }
+        }
+        answer += max;
+    }
+
+    return answer;
+}
+
+int main()
+{
+    cout << solution("4177252841", 4);
+
+	return 0;
+}
