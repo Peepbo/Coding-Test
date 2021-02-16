9b24a672e7fadedcbaa0dffdea04274bc90ac395
Peepbo <tngus10778@gmail.com>
2021-02-16 11:46:28 +0900

    Programmers - 멀쩡한 사각형

---
+++ Programmers/Level 2/멀쩡한 사각형/CodingTest/CodingTest.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int gcd(int a, int b)
+{
+	int c;
+	if (b > a)
+	{
+		c = a;
+		a = b;
+		b = c;
+	}
+
+	while (b != 0)
+	{
+		c = a % b;
+		a = b;
+		b = c;
+	}
+	return a;
+}
+
+long long solution(int w, int h) {
+	long long max = (long long)w * h;
+	
+	int div = gcd(w, h);
+
+	long long bW = w / div;
+	long long bH = h / div;
+
+	long long bNum = (bW + bH - 1) * div;
+
+	return max - bNum;
+}
+
+int main()
+{
+	cout << solution(10, 5);
+
+	return 0;
+}
