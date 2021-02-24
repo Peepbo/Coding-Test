aba1bafdc4813df83ac932f2baecc9e1a1b78f98
Peepbo <tngus10778@gmail.com>
2021-02-25 03:15:25 +0900

    Programmers - 다음 큰 숫자

---
+++ Programmers/Level 2/다음 큰 숫자/CodingTest/CodingTest.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+	int num = n;
+	int count = 0, cpCnt = 0;
+
+	while (n > 0)
+	{
+		int t = n % 2;
+		n /= 2;
+
+		if (t == 1)count++;
+	}
+
+	while (count != cpCnt)
+	{
+		num++;
+		cpCnt = 0;
+
+		int cpNum = num;
+		while (cpNum > 0)
+		{
+			int t = cpNum % 2;
+			cpNum /= 2;
+			if (t == 1)cpCnt++;
+		}
+	}
+
+	return num;
+}
+
+int main()
+{
+	solution(15);
+
+	return 0;
+}
