50ed4645ca1c555933db7d6efeb1a5b8501ce9c6
Peepbo <tngus10778@gmail.com>
2021-02-26 17:09:29 +0900

    Programmers - 괄호 변환

---
+++ Programmers/Level 2/괄호 변환/CodingTest/CodingTest.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string Recursion(string str)
+{
+    if (str == "")return str;
+
+    int open = 0, close = 0;
+    
+    string u, v;
+
+    bool uWrite = true, Collect = true;
+
+    int len = str.size();
+    for (int i = 0; i < len; i++)
+    {
+        if (uWrite)
+        {
+            u += str[i];
+
+            if (str[i] == '(')open++;
+            else close++;
+
+            if (open < close) Collect = false;
+
+            else if (open == close) uWrite = false;
+        }
+
+        else v += str[i];
+    }
+
+    if (Collect)
+    {
+        return u + Recursion(v);
+    }
+    else
+    {
+        string temp;
+        temp = '(' + Recursion(v) + ')';
+
+        for (int i = 1; i < u.size() - 1; i++)
+        {
+            if (u[i] == '(') temp += ')';
+            else temp += '(';
+        }
+
+        return temp;
+    }
+}
+
+string solution(string p) {
+    return Recursion(p);
+}
+
+int main()
+{
+    cout << solution("(()())()");
+
+	return 0;
+}
