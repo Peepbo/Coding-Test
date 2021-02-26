504e93dd251a0567cce6929f543ae661e545d65a
Peepbo <tngus10778@gmail.com>
2021-02-26 19:05:47 +0900

    Programmers - JadenCase 문자열 만들기

---
+++ Programmers/Level 2/JadenCase 문자열 만들기/CodingTest/CodingTest.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    bool Big = true;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == ' ')
+        {
+            Big = true;
+            continue;
+        }
+
+        if (Big)
+        {
+            s[i] = toupper(s[i]);
+            Big = false;
+        }
+        else s[i] = tolower(s[i]);
+    }
+
+    return s;
+}
+
+int main()
+{
+    cout << solution("3people unFollowed me");
+
+	return 0;
+}
