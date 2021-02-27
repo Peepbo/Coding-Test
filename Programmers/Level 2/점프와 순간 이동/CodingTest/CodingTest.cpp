aa5570e0f14d72b111a08d6209834a1920953bc0
Peepbo <tngus10778@gmail.com>
2021-02-27 19:36:42 +0900

    Programmers - 점프와 순간 이동

---
+++ Programmers/Level 2/점프와 순간 이동/CodingTest/CodingTest.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n)
+{
+    int ans = 0;
+    while (n > 0)
+    {
+        ans += n % 2;
+        n /= 2;
+    }
+
+    return ans;
+}
+
+int main()
+{
+    cout << solution(5);
+
+	return 0;
+}
