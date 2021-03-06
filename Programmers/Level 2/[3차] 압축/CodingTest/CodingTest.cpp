064a47419be68fd5c6ceec653e7c84e2d34ee392
Peepbo <tngus10778@gmail.com>
2021-03-06 16:50:42 +0900

    Programmers - [3차] 압축

---
+++ Programmers/Level 2/[3차] 압축/CodingTest/CodingTest.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+vector<int> solution(string msg) {
+    vector<int> answer;
+
+    unordered_map<string, int> mp;
+    string write, word;
+    for (int i = 0; i < 26; i++)
+    {
+        write = 'A' + i;
+        mp[write] = i + 1;
+    }
+
+    write = msg[0];
+
+    int len = msg.size();
+    for (int i = 1; i <= len; i++)
+    {
+        word = write + msg[i];
+        if (mp[word] == 0)
+        {
+            answer.emplace_back(mp[write]);
+            mp[word] = mp.size();
+
+            write = msg[i];
+        }
+        else
+        {
+            write += msg[i];
+        }
+    }
+
+    return answer;
+}
+
+int main()
+{
+    auto output = solution("ABABABABABABABAB");
+
+    for (auto& it : output) cout << it << '-';
+
+	return 0;
+}
