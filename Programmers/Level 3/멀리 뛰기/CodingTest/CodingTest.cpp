4e7ccbe8bc32e94f06d9f1311599a7ec319fc8bd
Peepbo <tngus10778@gmail.com>
2021-03-10 16:40:47 +0900

    Programmers - 멀리 뛰기

---
+++ Programmers/Level 3/멀리 뛰기/CodingTest/CodingTest.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <array>
+
+using namespace std;
+
+long long fib(int num)
+{
+    array<long long, 2> arr{ 0,1 };
+    
+    for (int i = 2; i < num + 1; i++)
+    {
+        arr[0] = (arr[0] + arr[1]) % 1234567;
+        swap(arr[0], arr[1]);
+    }
+
+    return arr.back();
+}
+
+long long solution(int n) {
+    return fib(n + 1);
+}
+
+int main()
+{
+    cout << solution(3);
+
+	return 0;
+}
