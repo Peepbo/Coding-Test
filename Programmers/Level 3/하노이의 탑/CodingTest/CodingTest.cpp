97befd737f35f417eee06d97944b5657f8e13b89
Peepbo <tngus10778@gmail.com>
2021-03-10 18:24:46 +0900

    Programmers - 하노이의 탑

---
+++ Programmers/Level 3/하노이의 탑/CodingTest/CodingTest.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> v;
+
+void hanoi(int n, int start, int mid, int end)
+{
+    if (n == 1)
+    {
+        v.emplace_back(vector<int>{ start, end });
+        return;
+    }
+
+    hanoi(n - 1, start, end, mid);
+    v.emplace_back(vector<int>{ start,end });
+    hanoi(n - 1, mid, start, end);
+}
+
+vector<vector<int>> solution(int n) {
+    vector<vector<int>> answer;
+    hanoi(n, 1, 2, 3);
+    
+    answer.assign(v.begin(), v.end());
+    return answer;
+}
+
+int main()
+{
+    solution(3);
+
+	return 0;
+}
