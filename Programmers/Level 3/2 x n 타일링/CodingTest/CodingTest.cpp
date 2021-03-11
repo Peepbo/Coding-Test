60afd61dcf4e99c17dc76de73a053e5d87790ec4
Peepbo <tngus10778@gmail.com>
2021-03-11 13:05:10 +0900

    Programmers - 2 x n 타일링

---
+++ Programmers/Level 3/2 x n 타일링/CodingTest/CodingTest.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+#include <array>
+
+using namespace std;
+
+int fib(int num)
+{
+    array<int, 2> arr{ 0,1 };
+
+    for (int i = 2; i < num + 1; i++)
+    {
+        arr[0] = (arr[0] + arr[1]) % 1000000007;
+        swap(arr[0], arr[1]);
+    }
+
+    return arr.back();
+}
+
+int solution(int n) {
+    return fib(n);
+}
+
+int main()
+{
+    solution(3);
+
+	return 0;
+}
