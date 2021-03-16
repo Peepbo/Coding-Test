0d2da54ddf00a355c43937124ab0b44af9f1afa8
Peepbo <tngus10778@gmail.com>
2021-03-16 16:02:04 +0900

    Programmers - 위장(압축)

---
+++ Programmers/Level 2/위장/CodingTest/CodingTest.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+int solution(vector<vector<string>> clothes) {
+	int answer = 1;
+	unordered_map<string, int> m;
+
+	for (const auto& it : clothes)
+		m[it[1]]++;
+
+	for (const auto& it : m)
+		answer *= it.second + 1;
+
+	return answer - 1;
+}
+
+int main()
+{
+    cout << solution({ 
+		{ "yellowhat", "headgear"},
+		{ "bluesunglasses", "eyewear" },
+		{ "green_turban", "headgear" } });
+
+    return 0;
+}
