5824250f2b99816c6f2ae48e3450676eb4030fa1
Peepbo <tngus10778@gmail.com>
2021-03-19 22:39:50 +0900

    Programmers - 피보나치 수

---
+++ Programmers/Level 2/피보나치 수/Opt/CodingTest/CodingTest.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+#include <array>
+
+int Fib(int n)
+{
+    std::array<int, 2> arr{ 0,1 };
+
+    for (int i = 2; i < n + 1; i++)
+    {
+        arr[0] = (arr[0] + arr[1]) % 1234567;
+        std::swap(arr[0], arr[1]);
+    }
+
+    return arr[1];
+}
+
+int solution(int n) {
+    return Fib(n);
+}
+
+int main()
+{
+
+	return 0;
+}
