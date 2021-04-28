791f93ed943137f534b48bdc596a47b6ca187098
Peepbo <tngus10778@gmail.com>
2021-04-28 23:44:12 +0900

    Programmers - 음양 더하기

---
+++ Programmers/Level 1/음양 더하기/CodingTest/CodingTest.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> absolutes, vector<bool> signs) {
+    int answer = 0, idx = 0;
+
+    for (const int& num : absolutes)
+    {
+        answer += (signs[idx++] ? num : -num);
+    }
+
+    return answer;
+}
+
+int main()
+{
+    cout << solution({ 4,7,12 }, { true,false,true });
+}
