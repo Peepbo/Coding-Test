e36f22c16799c1807af466ed282fa08a614a197c
Peepbo <tngus10778@gmail.com>
2021-05-17 16:36:54 +0900

    Programmers - 약수의 개수와 덧셈 [optimization]

---
+++ Programmers/Level 1/약수의 개수와 덧셈/0517/CodingTest/CodingTest.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int left, int right) {
+    int answer = 0, count;
+
+    for (int num = left; num <= right; num++)
+    {
+        count = 0;
+        for (int i = 1; i <= num / 2; i++)
+        {
+            if (num % i == 0)
+            {
+                if (i < num / i) count += 2;
+                else
+                {
+                    if (i == num / i)count++;
+
+                    break;
+                }
+            }
+        }
+
+        if (count % 2 == 0) answer += num;
+        else answer -= num;
+    }
+
+    return answer;
+}
+
+int main()
+{
+    solution(13, 17);
+}
