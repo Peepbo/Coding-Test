3dcea9589f04bd01fbcc6003be10fb918c1b5c9b
Peepbo <tngus10778@gmail.com>
2021-05-25 15:39:24 +0900

    Programmers - 짝지어 제거하기

---
+++ Programmers/Level 2/짝지어 제거하기/CodingTest0525/CodingTest.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+int solution(string s)
+{
+    if (s.size() % 2 != 0)return 0;
+
+    int answer = 1;
+    stack<char> checker;
+
+    for (const char& ch : s)
+    {
+        if (!checker.empty() && checker.top() == ch)
+            checker.pop();
+        else 
+            checker.push(ch);
+    }
+
+    return checker.empty()? 1 : 0;
+}
+
+int main()
+{
+    //cout << solution("baabaa");
+    cout << solution("cdcd");
+}
