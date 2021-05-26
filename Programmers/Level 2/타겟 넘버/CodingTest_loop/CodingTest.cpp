12d244f3cb3071e28e1599fd7816230fb29a15b7
Peepbo <tngus10778@gmail.com>
2021-05-26 18:40:54 +0900

    Programmers - 타겟 넘버 (반복문)

---
+++ Programmers/Level 2/타겟 넘버/CodingTest_loop/CodingTest.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+int solution(vector<int> numbers, int target) {
+    int answer = 0;
+
+    struct info
+    {
+        int index, sum;
+    };
+
+    stack<info> st;
+    st.push({ 0,0 });
+
+    while (!st.empty())
+    {
+        info now = st.top();
+        st.pop();
+
+        if (now.index < numbers.size())
+        {
+            st.push({ now.index + 1, now.sum + numbers[now.index] });
+            st.push({ now.index + 1, now.sum - numbers[now.index] });
+        }
+        else if(now.sum == target) answer++;
+    }
+
+    return answer;
+}
+
+int main()
+{
+    cout << solution({ 1,1,1,1,1 }, 3);
+}
