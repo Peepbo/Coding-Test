941c649878594d40df1df582ee1cad1876712c44
Peepbo <tngus10778@gmail.com>
2021-05-26 18:39:05 +0900

    Programmers - 타겟 넘버 (재귀)

---
+++ Programmers/Level 2/타겟 넘버/CodingTest_recursive/CodingTest.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int collect;
+int targetNumber;
+
+void dfs(int index, int sum, const vector<int>& numbers)
+{
+    if (index == numbers.size())
+    {
+        if (sum == targetNumber) collect++;
+
+        return;
+    }
+
+    dfs(index + 1, sum + numbers[index], numbers);
+    dfs(index + 1, sum - numbers[index], numbers);
+}
+
+int solution(vector<int> numbers, int target) {
+    targetNumber = target;
+
+    dfs(0, 0, numbers);
+
+    return collect;
+}
+
+int main()
+{
+    cout << solution({ 1,1,1,1,1 }, 3);
+}
