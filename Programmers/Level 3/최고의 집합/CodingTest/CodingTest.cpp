56b9a3aa2f4a63e4551d2b47b70e15b6cdf21b6a
Peepbo <tngus10778@gmail.com>
2021-05-30 18:31:26 +0900

    Programmers - 최고의 집합

---
+++ Programmers/Level 3/최고의 집합/CodingTest/CodingTest.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n, int s) {
+    vector<int> answer;
+
+    if (n > s) answer.emplace_back(-1);
+    else if (n == s) answer.assign(1, n);
+    else
+    {
+        while (n > 0)
+        {
+            answer.emplace_back(s / n);
+            s -= answer.back();
+
+            n--;
+        }
+    }
+
+    return answer;
+}
+
+int main()
+{
+    auto vec = solution(5,9);
+
+    for (auto& it : vec)
+        cout << it << endl;
+}
