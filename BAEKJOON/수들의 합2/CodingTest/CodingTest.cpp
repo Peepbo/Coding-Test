eb8a7d503052b4b4badf931cf4802fd7b92de738
Peepbo <tngus10778@gmail.com>
2021-06-07 20:05:15 +0900

    BAEKJOON - 수들의 합2 (투 포인터 알고리즘)

---
+++ BAEKJOON/수들의 합2/CodingTest/CodingTest.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int board[10001];
+
+int main()
+{
+	int N, M, sum = 0, collect = 0, s = 0, e = 0;
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++) 
+		cin >> board[i];
+
+	while (e <= N)
+	{
+		if (sum >= M)
+			sum -= board[s++];
+		else if (sum < M)
+			sum += board[e++];
+		if (sum == M)
+			collect++;
+	}
+
+	cout << collect;
+}
