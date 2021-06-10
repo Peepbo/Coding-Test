c1b5b14202a1a483169622028f2d3555361a58c8
Peepbo <tngus10778@gmail.com>
2021-06-10 19:10:37 +0900

    Programmers - [카카오 인턴] 키패드 누르기

---
+++ Programmers/Level 1/키패드 누르기/CodingTest/CodingTest.cpp
@@ -0,0 +1,42 @@
+﻿#include <algorithm>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> nums)
+{
+	int answer = 0;
+	int len = nums.size();
+	int halfLen = len / 2;
+
+	sort(nums.begin(), nums.end());
+	unique(nums.begin(), nums.end());
+
+	int before = nums.front();
+	for (int i = 1; i < len; i++)
+	{
+		if (before < nums[i])
+		{
+			if (i == halfLen)
+			{
+				answer = halfLen;
+				break;
+			}
+		}
+
+		else
+		{
+			answer = i;
+			break;
+		}
+
+		before = nums[i];
+	}
+	return answer;
+}
+
+int main()
+{
+
+}
