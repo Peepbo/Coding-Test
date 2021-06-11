bcab0f2098aad705fd6df4891e8eac81234871d3
Peepbo <tngus10778@gmail.com>
2021-06-11 18:28:03 +0900

    BAEKJOON - 두 용액 (투 포인터 알고리즘)

---
+++ BAEKJOON/두 용액/CodingTest/CodingTest.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#define INF 2000000000
+
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+
+	vector<int> v(N);
+
+	for (int i = 0; i < N; i++)
+		cin >> v[i];
+
+	sort(v.begin(), v.end());
+
+	int l = 0, r = N - 1, min = INF, output[2], sum;
+
+	while (l < r)
+	{
+		sum = v[l] + v[r];
+
+		if (abs(sum) < min)
+		{
+			min = abs(sum);
+			output[0] = v[l];
+			output[1] = v[r];
+		}
+
+		if (sum < 0)
+			l++;
+		else
+			r--;
+	}
+
+	printf("%d %d", output[0], output[1]);
+}
