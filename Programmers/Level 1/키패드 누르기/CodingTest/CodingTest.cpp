607ce6f4b7e6fd9f6651ebdf5223eb03803ab442
Peepbo <tngus10778@gmail.com>
2021-06-11 18:26:38 +0900

    Programmers - 키패드 누르기 (잘못올린 코드 수정)

--- Programmers/Level 1/키패드 누르기/CodingTest/CodingTest.cpp
+++ Programmers/Level 1/키패드 누르기/CodingTest/CodingTest.cpp
@@ -1,37 +1,79 @@
-﻿#include <algorithm>
-#include <string>
+﻿#include <string>
 #include <vector>
 
 using namespace std;
 
-int solution(vector<int> nums)
-{
-	int answer = 0;
-	int len = nums.size();
-	int halfLen = len / 2;
+string solution(vector<int> numbers, string hand) {
+	string answer = "";
 
-	sort(nums.begin(), nums.end());
-	unique(nums.begin(), nums.end());
+	string pad[4] = { {1,2,3}
+					,{4,5,6},
+					 {7,8,9},
+					 {10,0,20} };
 
-	int before = nums.front();
-	for (int i = 1; i < len; i++)
+	int arrL[2] = { 3,0 }; //상하,좌우
+	int arrR[2] = { 3,2 }; //상하,좌우
+
+	for (int i = 0; i < numbers.size(); i++)
 	{
-		if (before < nums[i])
+		if (numbers[i] % 3 == 1)
 		{
-			if (i == halfLen)
+			answer += 'L';
+			arrL[0] = numbers[i] / 3;
+			arrL[1] = 0;
+			continue;
+		}
+		if (numbers[i] % 3 == 0 && numbers[i] != 0)
+		{
+			answer += 'R';
+			arrR[0] = (numbers[i] / 3 - 1);
+			arrR[1] = 2;
+			continue;
+		}
+
+		int goal[2] = { 0,0 };
+		for (int k = 0; k < 4; k++)
+		{
+			for (int j = 0; j < 3; j++)
 			{
-				answer = halfLen;
-				break;
+				if (numbers[i] == pad[k][j])
+				{
+					goal[0] = k; // 상하
+					goal[1] = j; // 좌우
+					break;
+				}
 			}
 		}
+		int moveL = abs(goal[0] - arrL[0]) + abs(goal[1] - arrL[1]);
+		int moveR = abs(goal[0] - arrR[0]) + abs(goal[1] - arrR[1]);
 
-		else
+		if (moveL < moveR)
 		{
-			answer = i;
-			break;
+			answer += 'L';
+			arrL[0] = goal[0];
+			arrL[1] = goal[1];
 		}
-
-		before = nums[i];
+		else if (moveL > moveR)
+		{
+			answer += 'R';
+			arrR[0] = goal[0];
+			arrR[1] = goal[1];
+		}
+		else if (moveL == moveR)
+		{
+			if (hand == "right")
+			{
+				answer += 'R';
+				arrR[0] = goal[0];
+				arrR[1] = goal[1];
+			}
+			else
+			{
+				answer += 'L';
+				arrL[0] = goal[0];
+				arrL[1] = goal[1];
+			}
+		}
 	}
 	return answer;
 }
