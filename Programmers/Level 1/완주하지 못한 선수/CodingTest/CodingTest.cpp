ae7e61d755cda9d549181691928762d7949e9c82
Peepbo <tngus10778@gmail.com>
2021-06-12 23:03:02 +0900

    Programmers - 완주하지 못한 선수

---
+++ Programmers/Level 1/완주하지 못한 선수/CodingTest/CodingTest.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+	map<string, int> m1;
+
+	for (string name : participant)
+	{
+		m1[name]++;
+	}
+	for (string name : completion)
+	{
+		m1[name]--;
+	}
+	for (auto pair : m1)
+	{
+		if (pair.second > 0) return pair.first;
+	}
+}
+
+int main()
+{
+
+}
