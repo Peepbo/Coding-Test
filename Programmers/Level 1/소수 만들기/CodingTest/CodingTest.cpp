2a9dee143175370d9fecc0885230df2fb3d96b1e
Peepbo <tngus10778@gmail.com>
2021-06-14 22:03:12 +0900

    Programmers - 소수 만들기

---
+++ Programmers/Level 1/소수 만들기/CodingTest/CodingTest.cpp
@@ -0,0 +1,25 @@
+﻿#include <vector>
+
+using namespace std;
+bool check(int n) {
+    for (int i = 2; i < n / 2; i++)
+        if (n % i == 0)
+            return false;
+    return true;
+}
+int solution(vector<int> nums) {
+    int answer = 0;
+    int N = nums.size();
+    for (int i = 0; i < N; i++)
+        for (int j = i + 1; j < N; j++)
+            for (int k = j + 1; k < N; k++) {
+                if (check(nums[i] + nums[j] + nums[k]))
+                    answer++;
+            }
+    return answer;
+}
+
+int main()
+{
+
+}
