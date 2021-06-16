8f0375c695ddc294fcaaffacbebf0a765cb19c3e
Peepbo <tngus10778@gmail.com>
2021-06-16 23:29:22 +0900

    Programmers - 실패율

---
+++ Programmers/Level 1/실패율/CodingTest/CodingTest.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<int> solution(int N, vector<int> stages) {
+	vector<int> answer;
+	int* order = new int[N + 1];
+	int* number = new int[N + 1];//개수
+	double* rate = new double[N + 1];//실패율
+	for (int i = 1; i < N + 1; i++)
+	{
+		order[i] = i;
+		number[i] = 0;
+	}
+
+	for (int i = 1; i < N + 1; i++)
+	{
+		for (int j = 0; j < stages.size(); j++)
+		{
+			if (i == stages[j])number[i]++;
+
+		}
+	}
+
+	int maxNum = stages.size();
+	for (int i = 1; i < N + 1; i++)
+	{
+		if (maxNum <= 0)rate[i] = 0;
+		else
+		{
+			rate[i] = (double)number[i] / maxNum;
+			maxNum -= number[i];
+		}
+	}
+
+	for (int i = 1; i < N; i++)
+	{
+		for (int j = 2; j <= (N + 1) - i; j++)
+		{
+			if (rate[j] > rate[j - 1])
+			{
+				swap(rate[j], rate[j - 1]);
+				swap(order[j], order[j - 1]);
+			}
+		}
+	}
+	for (int i = 1; i < N + 1; i++) // 1,2,3,4,5
+	{
+		answer.push_back(order[i]);
+	}
+	return answer;
+}
+
+int main()
+{
+
+}
