4e7d3bbd2d6bfd0691a1d0d0c244e7c11745b11b
Peepbo <tngus10778@gmail.com>
2021-06-17 20:30:54 +0900

    Programmers - 예산

---
+++ Programmers/Level 1/예산/CodingTest/CodingTest.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> d, int budget) {
+	int answer = 0;
+	//정렬
+	for (int i = 0; i < d.size() - 1; i++)
+	{
+		for (int j = 1; j < d.size() - i; j++)
+		{
+			if (d[j - 1] > d[j]) swap(d[j - 1], d[j]);
+		}
+	}
+	//예산 계산
+	for (int i = 0; i < d.size(); i++)
+	{
+		if (budget < d[i])break;
+		else
+		{
+			budget -= d[i];
+			answer++;
+		}
+	}
+	return answer;
+}
+
+int main()
+{
+
+}
