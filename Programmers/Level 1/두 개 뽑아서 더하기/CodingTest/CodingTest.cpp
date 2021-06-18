f64d7c67f951f2a8e44580c11a0cf143efa9110d
Peepbo <tngus10778@gmail.com>
2021-06-18 21:54:12 +0900

    Programmers - 두 개 뽑아서 더하기

---
+++ Programmers/Level 1/두 개 뽑아서 더하기/CodingTest/CodingTest.cpp
@@ -0,0 +1,32 @@
+﻿#include <string>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+vector<int> solution(vector<int> numbers) {
+	vector<int> answer;
+
+	set<int> _set;
+	set<int>::iterator iter;
+
+	for (int i = 0; i < numbers.size() - 1; i++)
+	{
+		for (int j = i + 1; j < numbers.size(); j++)
+		{
+			_set.insert(numbers[i] + numbers[j]);
+		}
+	}
+
+	for (iter = _set.begin(); iter != _set.end(); ++iter)
+	{
+		answer.push_back(*iter);
+	}
+
+	return answer;
+}
+
+int main()
+{
+
+}
