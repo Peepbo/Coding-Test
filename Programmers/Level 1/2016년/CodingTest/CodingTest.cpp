85723f9d3098ee1b7e3d670816f38fc6d1827bab
Peepbo <tngus10778@gmail.com>
2021-06-19 20:41:21 +0900

    Programmers - 2016년

---
+++ Programmers/Level 1/2016년/CodingTest/CodingTest.cpp
@@ -0,0 +1,24 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int a, int b) {
+	string answer = "";
+	int day = b;
+	int Imonth[12] = { 31,29,31,30,31,30,31,31,30,31,30,31 };
+	string Sday[12] = { "FRI","SAT","SUN","MON","TUE","WED","THU" };
+
+	for (int i = 0; i < a - 1; i++)
+	{
+		day += Imonth[i];
+	}
+	day -= 1;
+	answer = Sday[day % 7];
+	return answer;
+}
+
+int main()
+{
+
+}
