968f6a3de461ea6732c7e357c6426aff6b1c75c8
Peepbo <tngus10778@gmail.com>
2021-06-21 21:38:56 +0900

    Programmers - 가운데 글자 가져오기

---
+++ Programmers/Level 1/가운데 글자 가져오기/CodingTest/CodingTest.cpp
@@ -0,0 +1,25 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+	string answer = "";
+	int order = 0;
+	if (s.size() % 2 != 0) // 홀수 일 때
+	{
+		order = s.size() / 2;
+		answer += s[order];
+	}
+	else // 짝수 일 때
+	{
+		order = s.size() / 2 - 1;
+		answer += s[order];
+		answer += s[order + 1];
+	}
+	return answer;
+}
+
+int main()
+{
+}
