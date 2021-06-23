5402975cd28892031b883bf58bc34ab46cd02581
Peepbo <tngus10778@gmail.com>
2021-06-23 22:30:51 +0900

    Programmers - 같은 숫자는 싫어

---
+++ Programmers/Level 1/같은 숫자는 싫어/CodingTest/CodingTest.cpp
@@ -0,0 +1,23 @@
+﻿#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr)
+{
+	vector<int> answer;
+	int inc = 1;
+	answer.push_back(arr[0]);
+	for (int i = 1; i < arr.size(); i++)
+	{
+		if (answer[i - inc] != arr[i]) answer.push_back(arr[i]);
+		else inc++;
+	}
+
+	return answer;
+}
+
+int main()
+{
+
+}
