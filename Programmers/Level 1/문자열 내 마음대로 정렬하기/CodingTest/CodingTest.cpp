0445099869c08b47ad35f99904fb96d3e3ded11e
Peepbo <tngus10778@gmail.com>
2021-06-27 22:41:50 +0900

    Programmers - 문자열 내 마음대로 정렬하기

---
+++ Programmers/Level 1/문자열 내 마음대로 정렬하기/CodingTest/CodingTest.cpp
@@ -0,0 +1,27 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int idx;
+
+bool compare(string x, string y)
+{
+	if (x[idx] < y[idx]) return true;
+	else if (x[idx] > y[idx])return false;
+
+	return x < y;
+}
+
+vector<string> solution(vector<string> strings, int n) {
+	idx = n;
+	sort(strings.begin(), strings.end(), compare);
+
+	return strings;
+}
+
+int main()
+{
+}
