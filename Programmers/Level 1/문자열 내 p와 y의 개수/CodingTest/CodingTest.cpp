630afad522e221e78b946fe4c29729ecf8127153
Peepbo <tngus10778@gmail.com>
2021-06-28 23:15:42 +0900

    Programmers - 문자열 내 p와 y의 개수

---
+++ Programmers/Level 1/문자열 내 p와 y의 개수/CodingTest/CodingTest.cpp
@@ -0,0 +1,23 @@
+﻿#include <string>
+#include <iostream>
+using namespace std;
+
+bool solution(string s)
+{
+	bool answer = true;
+	int pNum, yNum;
+	pNum = yNum = 0;
+
+	for (int i = 0; i < s.length(); i++)
+	{
+		if (s[i] == 'p' || s[i] == 'P')pNum++;
+		else if (s[i] == 'y' || s[i] == 'Y')yNum++;
+	}
+	if (pNum != yNum)answer = false;
+
+	return answer;
+}
+
+int main()
+{
+}
