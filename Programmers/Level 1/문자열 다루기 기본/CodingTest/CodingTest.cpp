eb46a2fa429a10c49b378cfad00f2b3fe521257a
Peepbo <tngus10778@gmail.com>
2021-07-03 22:42:28 +0900

    Programmers - 문자열 다루기 기본

---
+++ Programmers/Level 1/문자열 다루기 기본/CodingTest/CodingTest.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(string s) {
+	bool answer = true;
+	for (int i = 0; i < s.size(); i++)
+	{
+		if ((int)s[i] > 57 || (int)s[0] == 32) answer = false;
+	}
+	return answer;
+}
+
+int main()
+{
+}
