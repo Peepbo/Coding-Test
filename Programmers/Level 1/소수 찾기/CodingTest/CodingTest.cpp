60de20b88c4c2b72c981dc268c9109d315567c36
Peepbo <tngus10778@gmail.com>
2021-07-18 23:45:40 +0900

    Programmers - 소수 찾기

---
+++ Programmers/Level 1/소수 찾기/CodingTest/CodingTest.cpp
@@ -0,0 +1,35 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+	int answer = 0;
+	vector<int> numbers(n + 1);
+	for (int i = 2; i <= n; i++)
+	{
+		numbers[i] = i;
+	}
+
+	for (int i = 2; i <= n; i++)
+	{
+		if (numbers[i] == 0)continue;
+
+		for (int j = i * 2; j <= n; j += i)
+		{
+			numbers[j] = 0;
+		}
+	}
+	for (int i = 2; i <= n; i++)
+	{
+		if (numbers[i] != 0)answer++;
+	}
+	return answer;
+}
+
+int main()
+{
+
+
+	return 0;
+}
