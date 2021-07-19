b5eb81e04de7dcd1b54c550bcbcbf0113f5b8f3c
Peepbo <tngus10778@gmail.com>
2021-07-19 19:07:31 +0900

    Programmers - 수박수박수박수박수박수?

---
+++ Programmers/Level 1/수박수박수박수박수박수/CodingTest/CodingTest.cpp
@@ -0,0 +1,19 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int n) {
+	string answer;
+	string waterMelon[2]{ "수","박" };
+
+	for (int i = 0; i < n; i++){
+		answer += waterMelon[i % 2];
+	}
+	return answer;
+}
+
+int main()
+{
+	return 0;
+}
