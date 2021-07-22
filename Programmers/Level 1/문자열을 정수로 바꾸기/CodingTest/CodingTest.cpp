3e9e697a83aa19f957006eebdaf4bd77cdd0134b
Peepbo <tngus10778@gmail.com>
2021-07-22 22:39:45 +0900

    Programmers - 문자열을 정수로 바꾸기

---
+++ Programmers/Level 1/문자열을 정수로 바꾸기/CodingTest/CodingTest.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool isDigit(const char& ch) {
+	return ch >= '0' && ch <= '9';
+}
+
+int solution(string s) {
+	int answer = 0, startIndex, ten = 10;
+	bool isPositive = s[0] == '-' ? false : true;
+	startIndex = isDigit(s[0]) ? 0 : 1;
+
+	for (int index = startIndex; index < s.size(); index++) {
+		answer *= ten;
+		answer += (s[index] - '0');
+	}
+
+	return isPositive ? answer : -answer;
+}
+
+int main() {
+	cout << solution("+1234") << endl;
+	cout << solution("-1234") << endl;
+
+	return 0;
+}
