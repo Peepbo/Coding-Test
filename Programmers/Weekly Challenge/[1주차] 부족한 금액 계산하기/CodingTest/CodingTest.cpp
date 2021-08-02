207264403fb3a04206026385f0b9dd5c794d1421
Peepbo <tngus10778@gmail.com>
2021-08-02 21:40:38 +0900

    Programmers - [1주차] 부족한 금액 계산하기

---
+++ Programmers/Weekly Challenge/[1주차] 부족한 금액 계산하기/CodingTest/CodingTest.cpp
@@ -0,0 +1,19 @@
+﻿using namespace std;
+
+long long solution(int price, int money, int count)
+{
+	long long need = 0, nextPrice = 0;
+
+	while (count--) {
+		nextPrice += price;
+		need += nextPrice;
+	}
+
+	return money - need >= 0 ? 0 : need - money;
+}
+
+int main() {
+	solution(3, 20, 4);
+
+	return 0;
+}
