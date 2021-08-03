37089832d491b21fcb26ee55ff23904298525a9e
Peepbo <tngus10778@gmail.com>
2021-08-03 23:50:24 +0900

    Programmers - 약수의 합

---
+++ Programmers/Level 1/약수의 합/CodingTest/CodingTest.cpp
@@ -0,0 +1,20 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+	int answer = 0;
+	for (int i = 1; i <= (n / 2); i++)
+	{
+		if (n % i == 0) answer += i;
+	}
+	answer += n;
+
+	return answer;
+}
+
+int main() {
+
+	return 0;
+}
