df6771344cb4e36c05fda2bd029d39927b299d1f
Peepbo <tngus10778@gmail.com>
2021-08-04 23:49:27 +0900

    Programmers - 이상한 문자 만들기

---
+++ Programmers/Level 1/이상한 문자 만들기/CodingTest/CodingTest.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    bool Big = true;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == ' ')
+        {
+            Big = true;
+            continue;
+        }
+
+        if (Big)
+        {
+            s[i] = toupper(s[i]);
+            Big = false;
+        }
+        else s[i] = tolower(s[i]);
+    }
+
+    return s;
+}
+
+int main() {
+
+	return 0;
+}
