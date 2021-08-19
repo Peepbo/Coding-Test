f994b1fd4c1dc98057f2bee08f306ac8f3555e9a
Peepbo:PC <tngus10778@gmail.com>
2021-08-19 22:45:37 +0900

    Programmers - 자릿수 뒤집어 배열로 만들기

---
+++ Programmers/Level 1/자릿수 뒤집어 배열로 만들기/CodingTest/CodingTest.cpp
@@ -0,0 +1,20 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(long long n) {
+	vector<int> answer;
+	long long number = n;
+	while (number > 0)
+	{
+		answer.push_back(number % 10);
+		number /= 10;
+	}
+	return answer;
+}
+
+int main() {
+
+	return 0;
+}
