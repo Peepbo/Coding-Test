9cdbe5a9d6aad6c626c0fdcf6667d65f72997a70
Peepbo:PC <tngus10778@gmail.com>
2021-08-21 23:34:14 +0900

    Programmers - 정수 제곱근 판별

---
+++ Programmers/Level 1/정수 제곱근 판별/CodingTest/CodingTest.cpp
@@ -0,0 +1,25 @@
+﻿#include <string>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+long long solution(long long n) {
+	long long answer = 0;
+
+	double cul = (double)n / 2;
+	long long sub = 1;
+	while (cul - sub > 0)
+	{
+		cul -= sub;
+		sub++;
+	}
+	if (cul * 2 == double(sub))answer = (sub + 1) * (sub + 1);
+	else answer = -1;
+
+	return answer;
+}
+
+int main()
+{
+
+}
