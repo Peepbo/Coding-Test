e3d013bdbc6d2e2b3f5fcdb19a740a5f53a698e9
Peepbo:PC <tngus10778@gmail.com>
2021-08-22 23:38:16 +0900

    Programmers - 제일 작은 수 제거하기

---
+++ Programmers/Level 1/제일 작은 수 제거하기/CodingTest/CodingTest.cpp
@@ -0,0 +1,31 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    int minNumber = arr.front(), minNumberIndex = 0, index = 0;
+
+    for (const int& number : arr)
+    {
+        if (number < minNumber)
+        {
+            minNumber = number;
+            minNumberIndex = index;
+        }
+
+        index++;
+    }
+
+    arr.erase(arr.begin() + minNumberIndex);
+
+    if (arr.empty())
+        arr.emplace_back(-1);
+
+    return arr;
+}
+
+int main()
+{
+
+}
