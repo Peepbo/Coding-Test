54c65bc85b48d00b83dc76a94522d63e091f961f
Peepbo:PC <tngus10778@gmail.com>
2021-08-23 23:32:08 +0900

    Programmers - 짝수와 홀수

---
+++ Programmers/Level 1/짝수와 홀수/CodingTest/CodingTest.cpp
@@ -0,0 +1,16 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int num) {
+	string answer = "";
+	if (num % 2 == 0)answer = "Even";
+	else answer = "Odd";
+	return answer;
+}
+
+int main()
+{
+
+}
