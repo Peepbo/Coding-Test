46f9e80a15017d2d4ed697ecb57fa29a01ea7f37
Peepbo:PC <tngus10778@gmail.com>
2021-09-08 22:22:05 +0900

    Programmers - 최대공약수와 최소공배수

---
+++ Programmers/Level 1/최대공약수와 최소공배수/CodingTest/CodingTest.cpp
@@ -0,0 +1,45 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n, int m) {
+	vector<int> answer;
+	int num = n;
+	while (num != 0)
+	{
+		if (m % num == 0)
+		{
+			answer.push_back(num);
+			break;
+		}
+		else num--;
+		while (n % num != 0) num--;
+	}
+	num = 1;
+	int cul = m, inc = 1;
+	while (true)
+	{
+		if (n * num < cul)
+		{
+			num++;
+			continue;
+		}
+		else if (n * num == cul)
+		{
+			answer.push_back(cul);
+			break;
+		}
+		else
+		{
+			num = 1;
+			inc++;
+			cul = m * inc;
+		}
+	}
+	return answer;
+}
+
+int main() {
+	return 0;
+}
