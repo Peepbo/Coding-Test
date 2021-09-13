add2b7acd0017b94dc8895282ab2c4e10b53079e
Peepbo:PC <tngus10778@gmail.com>
2021-09-13 21:57:52 +0900

    Programmers - 없는 숫자 더하기

---
+++ Programmers/Level 1/없는 숫자 더하기/CodingTest/CodingTest.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> numbers) {
+    int answer = 9 * 10 / 2;
+
+    for (const int& num : numbers) {
+        answer -= num;
+    }
+    return answer;
+}
+
+int main() {
+
+	return 0;
+}
