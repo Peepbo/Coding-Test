ed1ad9ac45d22287340bf12e8417233f5b1f93e7
Peepbo:PC <tngus10778@gmail.com>
2021-09-14 19:15:46 +0900

    Programmers - [7주차] 입실 퇴실 (필요없는 내용 제거 및 최적화)

--- Programmers/Weekly Challenge/[7주차] 입실 퇴실/CodingTest/CodingTest.cpp
+++ Programmers/Weekly Challenge/[7주차] 입실 퇴실/CodingTest/CodingTest.cpp
@@ -9,31 +9,27 @@ using namespace std;
 vector<int> solution(vector<int> enter, vector<int> leave) {
     const int size = enter.size();
     int eIdx = 0, lIdx = 0, newPerson, outPerson;
-    vector<vector<bool>> metList(size + 1, vector<bool>(size + 1, false));
     vector<int> answer(size);
     set<int> room;
 
     room.insert(enter[eIdx++]);
 
     while (eIdx < size) {
-        if (room.find(leave[lIdx]) == room.end()) {
-            newPerson = enter[eIdx++];
-
-            for (const auto& inPerson : room) {
-                if (!metList[inPerson][newPerson]) {
-                    metList[inPerson][newPerson] = metList[newPerson][inPerson] = true;
-
-                    answer[inPerson - 1]++;
-                    answer[newPerson - 1]++;
-                }
-            }
-
-            room.insert(newPerson);
-        }
-        else {
+        if (room.find(leave[lIdx]) != room.end()) {
             outPerson = leave[lIdx++];
 
             room.erase(outPerson);
+        }
+
+        else {
+            newPerson = enter[eIdx++];
+
+            for (const auto& inPerson : room) {
+                answer[inPerson - 1]++;
+                answer[newPerson - 1]++;
+            }
+
+            room.insert(newPerson);
         }
     }
 
