5791a9cef9f3440a57af996d04df02b4bad65b8b
Peepbo:PC <tngus10778@gmail.com>
2021-09-15 23:05:52 +0900

    Programmers - 콜라츠 추측

---
+++ Programmers/Level 1/콜라츠 추측/CodingTest/CodingTest.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num) {
+	int count = 0;
+	long long number = num;
+	while (number != 1)
+	{
+		if (number % 2 == 0)number /= 2;
+		else number = (number * 3) + 1;
+		count++;
+		if (count >= 500)
+		{
+			count = -1;
+			break;
+		}
+	}
+	return count;
+}
+
+int main() {
+	return 0;
+}
