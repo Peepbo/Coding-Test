4cf1edc78c1c87011b8cf07c26d1d0543c7f9b20
Peepbo:PC <tngus10778@gmail.com>
2021-09-16 19:48:59 +0900

    Programmers - 평균 구하기

---
+++ Programmers/Level 1/평균 구하기/CodingTest/CodingTest.cpp
@@ -0,0 +1,16 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+double solution(vector<int> arr) {
+    double answer = 0;
+    for (const int& num : arr) {
+        answer += num;
+    }
+    return answer / arr.size();
+}
+
+int main() {
+	return 0;
+}
