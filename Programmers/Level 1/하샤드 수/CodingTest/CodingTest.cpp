368e5fa6f15331c97758d9c66a2835a1cb94f306
Peepbo:PC <tngus10778@gmail.com>
2021-09-20 21:45:59 +0900

    Programmers - 하샤드 수

---
+++ Programmers/Level 1/하샤드 수/CodingTest/CodingTest.cpp
@@ -0,0 +1,24 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(int x) {
+	bool answer = true;
+
+	int cul = 0;
+	int num = x;
+	while (num > 0)
+	{
+		cul += num % 10;
+		num /= 10;
+	}
+	if (x % cul != 0)answer = false;
+
+	return answer;
+}
+
+int main() {
+
+	return 0;
+}
