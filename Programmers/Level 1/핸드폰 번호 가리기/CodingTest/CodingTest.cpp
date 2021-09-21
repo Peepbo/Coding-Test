228b37a1f3f745de86191ed1b2d6add33dc18918
Peepbo:PC <tngus10778@gmail.com>
2021-09-21 23:24:36 +0900

    Programmers - 핸드폰 번호 가리기

---
+++ Programmers/Level 1/핸드폰 번호 가리기/CodingTest/CodingTest.cpp
@@ -0,0 +1,17 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string phone_number) {
+	const size_t starLength = phone_number.size() - 4;
+	const string star(starLength, '*');
+	phone_number.replace(0, starLength, star);
+
+	return phone_number;
+}
+
+int main() {
+
+	return 0;
+}
