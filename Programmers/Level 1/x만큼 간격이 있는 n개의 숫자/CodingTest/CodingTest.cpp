3722dd42725f320efb201d230e85edd9bd6d6bdb
Peepbo:PC <tngus10778@gmail.com>
2021-09-23 16:37:33 +0900

    Programmers - x만큼 간격이 있는 n개의 숫자

---
+++ Programmers/Level 1/x만큼 간격이 있는 n개의 숫자/CodingTest/CodingTest.cpp
@@ -0,0 +1,19 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<long long> solution(int x, int n) {
+    vector<long long> answer(n);
+
+    for (int16_t i = 0; i < n; i++) {
+        answer[i] = (long long)x * (i + 1);
+    }
+
+    return answer;
+}
+
+int main() {
+
+	return 0;
+}
