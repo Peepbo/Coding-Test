fb1d5867dd2ea89564899d1c0e4a7c257be9a439
Peepbo:PC <tngus10778@gmail.com>
2021-09-27 17:33:29 +0900

    Programmers - [8주차] 최소직사각형

---
+++ Programmers/Weekly Challenge/[8주차] 최소직사각형/CodingTest/CodingTest.cpp
@@ -0,0 +1,26 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> sizes) {
+    int hW = 1, hH = 1, w, h;
+
+    for (const auto& vec : sizes) {
+        w = vec[0];
+        h = vec[1];
+
+        if (w < h)swap(w, h);
+
+        hW = max(hW, w);
+        hH = max(hH, h);
+    }
+
+    return hW * hH;
+}
+
+int main(void) {
+    solution({ { 60,50 }, { 30,70 }, { 60,30 }, { 80,40 } });
+
+	return 0;
+}
