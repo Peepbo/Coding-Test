d2e3fb91f04cd451c78a6cdf62c1f8a145107ae7
Peepbo:PC <tngus10778@gmail.com>
2021-09-28 23:39:51 +0900

    Programmers - 주식가격

---
+++ Programmers/Level 2/주식가격/CodingTest/CodingTest.cpp
@@ -0,0 +1,32 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> numbers) {
+	vector<int> answer;
+
+	for (int i = 0; i < numbers.size() - 1; i++)
+	{
+		int count = 0;
+		for (int j = i + 1; j < numbers.size(); j++)
+		{
+			if (numbers[i] > numbers[j])
+			{
+				count++;
+				break;
+			}
+
+			count++;
+		}
+		answer.push_back(count);
+	}
+	answer.push_back(0);
+
+	return answer;
+}
+
+int main(void) {
+
+	return 0;
+}
