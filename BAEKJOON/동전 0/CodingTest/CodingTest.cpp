b3ca96330a2ff35a0a68d4e27ccd72c5d301c863
Peepbo:PC <tngus10778@gmail.com>
2021-09-30 23:39:26 +0900

    BAEKJOON - 동전 0

---
+++ BAEKJOON/동전 0/CodingTest/CodingTest.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void) {
+	int N, K, num = 0;
+	cin >> N >> K;
+
+	vector<int> coins(N);
+	for (int i = 0; i < N; i++) {
+		cin >> coins[N - (i + 1)];
+	}
+
+	for (const int& coin : coins) {
+		if (K == 0)break;
+
+		if (K >= coin) {
+			num += K / coin;
+			K %= coin;
+		}
+	}
+
+	cout << num;
+
+	return 0;
+}
