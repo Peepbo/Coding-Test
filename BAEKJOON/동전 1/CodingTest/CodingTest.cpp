6579592ed9c941de046fae985fca81fa0195ce45
Peepbo:PC <tngus10778@gmail.com>
2021-10-01 02:02:38 +0900

    Programmers - 동전 1

---
+++ BAEKJOON/동전 1/CodingTest/CodingTest.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void) {
+	int n, k;
+	cin >> n >> k;
+
+	vector<int> coin(n);
+	vector<int> dp(k + 1);
+
+	for (int i = 0; i < n; i++) {
+		cin >> coin[i];
+	}
+
+	dp[0] = 1;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = coin[i]; j <= k; j++) {
+			dp[j] += dp[j - coin[i]];
+		}
+	}
+
+	cout << dp[k];
+
+	return 0;
+}
