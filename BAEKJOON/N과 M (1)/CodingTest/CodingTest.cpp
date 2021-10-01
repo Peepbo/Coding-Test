fa2242978377e58603b55d92f3aedd6cf5916bf8
Peepbo:PC <tngus10778@gmail.com>
2021-10-02 01:16:10 +0900

    BAEKJOON - N과 M (1)

---
+++ BAEKJOON/N과 M (1)/CodingTest/CodingTest.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+int MaxSize, MaxRange;
+
+set<vector<int>> output;
+
+void bfs(vector<int>& num, int bit) {
+	if (num.size() == MaxSize) {
+		output.insert(num);
+		return;
+	}
+
+	for (int i = 1; i <= MaxRange; i++) {
+		if (bit & (1 << i))continue;
+
+		num.emplace_back(i);
+		bfs(num, bit | (1 << i));
+		num.pop_back();
+	}
+}
+
+int main(void) {
+	int N, M;
+	cin >> N >> M;
+
+	MaxSize = M;
+	MaxRange = N;
+
+	for (int i = 1; i <= N; i++) {
+		vector<int> vec{ i };
+		bfs(vec, (1 << i));
+	}
+
+	for (const auto& v : output) {
+		for (const auto& i : v) {
+			cout << i << ' ';
+		}
+		cout << '\n';
+	}
+
+	return 0;
+}
