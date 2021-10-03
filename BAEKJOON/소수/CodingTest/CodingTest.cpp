908429bb03178d28a29d6c7498ba4dc17a6d5c4d
Peepbo:PC <tngus10778@gmail.com>
2021-10-03 20:40:59 +0900

    BAEKJOON - 소수 (에라토스테네스의 체)

---
+++ BAEKJOON/소수/CodingTest/CodingTest.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void) {
+	int M, N, sum = 0, minNum = 50000;
+	cin >> M >> N;
+
+	vector<bool> prime(10001, true);
+	prime[0] = prime[1] = false;
+
+	for (int i = 2; i <= 5000; i++) {
+		if (prime[i] == false)continue;
+
+		if (i > N) break;
+
+		for (int j = i * 2; j <= 10000; j += i) {
+			prime[j] = false;
+		}
+	}
+	
+	for (int i = M; i <= N; i++) {
+		if (prime[i]) {
+			sum += i;
+			minNum = min(minNum, i);
+		}
+	}
+
+	if (sum == 0)
+		printf("-1");
+	else 
+		printf("%d\n%d", sum, minNum);
+
+	return 0;
+}
