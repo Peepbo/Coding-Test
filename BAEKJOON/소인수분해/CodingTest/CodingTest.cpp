38566a3d6e0d28f9a649c673454980389567ec3e
Peepbo:PC <tngus10778@gmail.com>
2021-10-04 21:40:50 +0900

    BAEKJOON - 소인수분해

---
+++ BAEKJOON/소인수분해/CodingTest/CodingTest.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	int N, num;
+	
+	cin >> N;
+
+	while (N > 1) {
+		num = 2;
+		while (N % num != 0) {
+			num++;
+		}
+
+		cout << num << '\n';
+		N /= num;
+	}
+
+	return 0;
+}
