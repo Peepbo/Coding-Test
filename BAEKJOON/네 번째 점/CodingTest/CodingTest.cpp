a885f288cb299507998b321b57813a0d5413823d
Peepbo:PC <tngus10778@gmail.com>
2021-10-04 20:13:20 +0900

    BAEKJOON - 네 번째 점

---
+++ BAEKJOON/네 번째 점/CodingTest/CodingTest.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+int main(void) {
+	int x, y;
+	vector<pair<int, int>> pos(3);
+	set<pair<int, int>> s;
+
+	for (pair<int, int>& p : pos) {
+		cin >> p.first >> p.second;
+		s.insert(p);
+	}
+
+	for (int i = 0; i < 3; i++) {
+		x = pos[i].first;
+
+		for (int j = 0; j < 3; j++) {
+			y = pos[j].second;
+
+			if (s.find({ x,y }) == s.end()) {
+				printf("%d %d", x, y);
+				return 0;
+			}
+		}
+	}
+
+	return 0;
+}
