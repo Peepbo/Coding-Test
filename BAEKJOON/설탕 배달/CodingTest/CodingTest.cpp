0b8f710c5e7ca8b7a7f104b21d62931a70eccb89
Peepbo:PC <tngus10778@gmail.com>
2021-10-05 23:45:35 +0900

    BAEKJOON - 설탕 배달

---
+++ BAEKJOON/설탕 배달/CodingTest/CodingTest.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int N, count = 0;
+	cin >> N;
+	while (true)
+	{
+		if (N % 5 == 0) {
+			N -= 5;
+			count++;
+		}
+		else if (N % 3 == 0) {
+			N -= 3;
+			count++;
+		}
+		else if (N - 5 >= 0) {
+			N -= 5;
+			count++;
+		}
+		else N -= 3;
+
+		if (N > 0 && N < 3) {
+			count = -1;
+			break;
+		}
+		else if (N == 0) break;
+	}
+	cout << count;
+}
