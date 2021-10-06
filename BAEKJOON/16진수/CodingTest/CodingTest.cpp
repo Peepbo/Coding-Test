a2389d2b9d3fa93b9879377e5dd803b5f8c39493
Peepbo:PC <tngus10778@gmail.com>
2021-10-07 02:04:11 +0900

    BAEKJOON -  16진수

---
+++ BAEKJOON/16진수/CodingTest/CodingTest.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	string N;
+	int answer = 0, count = 1;
+	
+	cin >> N;
+
+	for (int i = N.size() - 1; i >= 0; i--) {
+		if (N[i] != '0') {
+			if (N[i] >= 'A') {
+				answer += (N[i] - 'A' + 10) * count;
+			}
+			else {
+				answer += (N[i] - '0') * count;
+			}
+		}
+		count *= 16;
+	}
+
+	cout << answer;
+
+	return 0;
+}
