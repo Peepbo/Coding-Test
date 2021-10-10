b39d4afee6df8d8d90fd0ae992634098bd42c2b0
Peepbo:PC <tngus10778@gmail.com>
2021-10-10 09:23:28 +0900

    BAEKJOON -  직사각형에서 탈출

---
+++ BAEKJOON/직사각형에서 탈출/CodingTest/CodingTest.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	int x, y, w, h, case1, case2;
+	cin >> x >> y >> w >> h;
+
+	case1 = min(x, y);
+	case2 = min(abs(x - w), abs(y - h));
+
+	cout << min(case1, case2);
+
+	return 0;
+}
