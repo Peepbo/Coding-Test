6a7051f54fa7ffa575b7c31da231ad1d56710167
Peepbo:PC <tngus10778@gmail.com>
2021-10-14 16:58:48 +0900

    BAEKJOON - 택시 기하학

---
+++ BAEKJOON/택시 기하학/CodingTest/CodingTest.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+#define PI 3.14159265359
+
+int main(void) {
+	double R;
+	cin >> R;
+	const double R2 = R * R;
+
+	printf("%lf\n%lf", PI * R2, R2 * 2);
+
+	return 0;
+}
