24c07a535ecc473cb15c531b98d857ebc8b63867
Peepbo:PC <tngus10778@gmail.com>
2021-10-16 22:29:54 +0900

    BAEKJOON - 직각삼각형

---
+++ BAEKJOON/직각삼각형/CodingTest/CodingTest.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	long long A, B, C;
+
+	while (true) {
+		cin >> A >> B >> C;
+		if (A == 0 && B == 0 && C == 0) break;
+		
+		if (C < A)swap(C, A);
+		if (C < B)swap(C, B);
+		
+		if (A * A + B * B == C * C) {
+			cout << "right" << '\n';
+		}
+		else {
+			cout << "wrong" << '\n';
+		}
+	}
+
+	return 0;
+}
