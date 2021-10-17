3dd8355ecab9446db24ec8157e4cb99eeba64e2d
Peepbo:PC <tngus10778@gmail.com>
2021-10-17 22:38:24 +0900

    BAEKJOON - 체스판 다시 칠하기

---
+++ BAEKJOON/체스판 다시 칠하기/CodingTest/CodingTest.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void) {
+	int N, M, minDraw = 64;
+	cin >> N >> M;
+	vector<string> board(N);
+
+	for (int i = 0; i < N; i++) {
+		cin >> board[i];
+	}
+
+	for (int i = 0; i < N; i++) { //start y
+		for (int j = 0; j < M; j++) { //start x
+
+			if (i + 8 > N)break;
+			if (j + 8 > M)continue;
+
+			int need1 = 0, need2 = 0; //W first, B first
+
+			for (int k = i; k < i + 8; k++) { //continue y
+				for (int l = j; l < j + 8; l++) { //continue x
+
+					if (k % 2 == l % 2) {
+						if (board[k][l] != 'W') {
+							need1++;
+						}
+						else {
+							need2++;
+						}
+					}
+					else {
+						if (board[k][l] != 'B') {
+							need1++;
+						}
+						else {
+							need2++;
+						}
+					}
+				}
+			}
+
+			minDraw = min(minDraw, min(need1, need2));
+		}
+	}
+
+	cout << minDraw;
+	return 0;
+}
