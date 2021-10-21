63b3aa8a13d124ec72c3e478283f6a76a97076f7
Peepbo:PC <tngus10778@gmail.com>
2021-10-21 12:55:33 +0900

    Programmers - 나머지가 1이 되는 수 찾기

---
+++ Programmers/Level 1/나머지가 1이 되는 수 찾기/CodingTest/CodingTest.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    const int half = n / 2;
+    vector<pair<bool, int>> decimal(n + 1, { true, 0 });
+    decimal[0] = decimal[1] = { false, 1 };
+
+    for (int i = 2; i <= half; i++) {
+        for (int j = i * 2; j <= n; j += i) {
+            if (decimal[j].second != 0)continue;
+
+            decimal[j] = { false, i };
+        }
+    }
+
+    return decimal[n - 1].first ? n - 1 : decimal[n - 1].second;
+}
+
+int main(void) {
+    for (int i = 3; i <= 100; i++) {
+        cout << i << ':' << solution(i) << '\n';
+    }
+
+	return 0;
+}
