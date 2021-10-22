70b61688267bdf5a57d87bb4c26689d2a85e27db
Peepbo:PC <tngus10778@gmail.com>
2021-10-22 20:57:51 +0900

    BAEKJOON - 카드2

---
+++ BAEKJOON/카드2/CodingTest/CodingTest.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <deque>
+
+using namespace std;
+
+int main(void) {
+	int N;
+	cin >> N;
+
+	deque<int> dq;
+	for (int i = 1; i <= N; i++) {
+		dq.push_back(i);
+	}
+
+	while (--N) {
+		dq.pop_front();
+		dq.push_back(dq.front());
+		dq.pop_front();
+	}
+
+	cout << dq.front();
+	return 0;
+}
