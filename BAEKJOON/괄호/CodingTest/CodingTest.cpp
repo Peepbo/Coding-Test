b3bc3de67c5e80c7ccb2be8107d75e25840a5eec
Peepbo:PC <tngus10778@gmail.com>
2021-10-23 14:59:05 +0900

    BAEKJOON - 괄호

---
+++ BAEKJOON/괄호/CodingTest/CodingTest.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+vector<string> message;
+
+void CheckVPS() {
+	stack<char> s;
+	string input;
+
+	cin >> input;
+	for (const char& ch : input) {
+		if (ch == '(')s.push(ch);
+		else {
+			if (!s.empty() && s.top() == '(')s.pop();
+			else {
+				message.emplace_back("NO");
+				return;
+			}
+		}
+	}
+
+	message.emplace_back(s.empty() ? "YES" : "NO");
+}
+
+int main(void) {
+	int T;
+	cin >> T;
+
+	while (T--) {
+		CheckVPS();
+	}
+
+	for (const string& str : message) {
+		cout << str << '\n';
+	}
+	
+	return 0;
+}
