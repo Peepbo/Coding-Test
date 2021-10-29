a4bf4a49e5728320cd7654d195040327d0f66d76
Peepbo:PC <tngus10778@gmail.com>
2021-10-30 00:24:48 +0900

    BAEKJOON - 01타일

---
+++ BAEKJOON/01타일/CodingTest/CodingTest.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int arr[2];
+
+int main(void) {
+	int N, temp;
+	cin >> N;
+
+	arr[0] = 1;
+	arr[1] = 1;
+	for (int i = 2; i <= N; i++) {
+		temp = arr[0] + arr[1];
+		arr[0] = arr[1];
+		arr[1] = temp % 15746;
+	}
+
+	cout << arr[1];
+	return 0;
+}
