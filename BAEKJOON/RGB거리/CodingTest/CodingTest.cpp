948b18e8883ab3649b864b32b926bea8a4593064
Peepbo:PC <tngus10778@gmail.com>
2021-10-31 05:58:55 +0900

    BAEKJOON - RGB거리

---
+++ BAEKJOON/RGB거리/CodingTest/CodingTest.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+
+using namespace std;
+
+#define INF 1000001
+
+int endSize, answer = INF;
+int arr[1001][3];
+int minValue[1001][3]; //idx,rgb
+
+void dfs(int rgb, int value, int idx) {
+	if (idx > endSize) {
+		answer = min(answer, value);
+		return;
+	}
+
+	for (int i = 0; i < 3; i++) {
+		if (rgb == i)continue;
+		if (minValue[idx][i] > value + arr[idx][i]) {
+			minValue[idx][i] = value + arr[idx][i];
+			dfs(i, minValue[idx][i], idx + 1);
+		}
+	}
+}
+
+int main(void) {
+	cin.sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int N;
+	cin >> N;
+	endSize = N;
+
+	int order = 1;
+	while (N--) {
+		for (int i = 0; i < 3; i++) {
+			cin >> arr[order][i];
+			minValue[order][i] = INF;
+		}
+		order++;
+	}
+
+	for (int i = 0; i < 3; i++) {
+		dfs(i, arr[1][i], 2);
+	}
+
+	cout << answer;
+	return 0;
+}
