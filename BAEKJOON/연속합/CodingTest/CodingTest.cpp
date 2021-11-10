73d9f8e1aa61d5cf664d5f6d90fe89c24d2d0272
Peepbo:PC <tngus10778@gmail.com>
2021-11-11 08:20:09 +0900

    BAEKJOON - 연속합

---
+++ BAEKJOON/연속합/CodingTest/CodingTest.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int dp[100001];
+
+int main(void) {
+	cin.sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int n, now, MCS; // MCS : Maximum Consecutive Sum
+	cin >> n >> now;
+	MCS = now;
+
+	dp[1] = now;
+	for (int i = 2; i <= n; i++) {
+		cin >> now;
+		dp[i] = max(dp[i - 1] + now, now);
+		MCS = max(MCS, dp[i]);
+	}
+
+	cout << MCS;
+	return 0;
+}
