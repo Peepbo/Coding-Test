612155305cc49fc595c6d5522bae2f466357687e
Peepbo:PC <tngus10778@gmail.com>
2021-11-14 05:24:37 +0900

    BAEKJOON - 오큰수

---
+++ BAEKJOON/오큰수/CodingTest/CodingTest.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+int main(void) {
+	stack<int> s; // idx
+	int A, idx = 0;
+	cin >> A;
+
+	vector<int> sequence(A), NGE(A, -1);
+	for (int i = 0; i < A; i++) {
+		cin >> sequence[i];
+	}
+
+	s.push(idx++);
+
+	while (idx < A) {
+		int now = sequence[idx];
+		int last = s.empty() ? now + 1 : sequence[s.top()];
+
+		if (last < now) {
+			NGE[s.top()] = now;
+			s.pop();
+		}
+		else {
+			s.push(idx++);
+		}
+	}
+
+	for (const int& num : NGE) {
+		cout << num << ' ';
+	}
+
+	return 0;
+}
