994ba452bbad87c89d5c4e695d2e7ae15e05129a
Peepbo:PC <tngus10778@gmail.com>
2021-11-15 09:39:47 +0900

    Programmers - n^2 배열 자르기

---
+++ Programmers/Level 2/n^2 배열 자르기/CodingTest/CodingTest.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n, long long left, long long right) {
+    vector<int> answer(right - left + 1);
+    int idx = 0;
+
+    while (left <= right) {
+        answer[idx] = max(left % n, left / n) + 1;
+
+        idx++;
+        left++;
+    }
+
+    return answer;
+}
+
+int main()
+{
+    solution(4, 7, 14);
+
+	return 0;
+}
