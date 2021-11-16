91f96f0c43f2762ddf1e0d679b803d1349aeb1ab
Peepbo:PC <tngus10778@gmail.com>
2021-11-16 11:57:38 +0900

    BAEKJOON - 수 정렬하기

---
+++ BAEKJOON/수 정렬하기/CodingTest/CodingTest.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> arr;
+
+void bubble_sort() {
+	for (int i = 0; i < arr.size(); i++) {
+		for (int j = 1; j < arr.size() - i; j++) {
+			if (arr[j] < arr[j - 1])
+				swap(arr[j], arr[j - 1]);
+		}
+	}
+}
+
+void print() {
+	for (const int& num : arr) {
+		cout << num << '\n';
+	}
+}
+
+int main()
+{
+	int N, idx = 0;
+	cin >> N;
+	arr.assign(N, 0);
+	while (N--) {
+		cin >> arr[idx++];
+	}
+
+	bubble_sort();
+
+	print();
+	return 0;
+}
