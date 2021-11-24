d8361fdf526245297f98562e89feec9ed7341648
Peepbo:PC <tngus10778@gmail.com>
2021-11-24 16:52:44 +0900

    BAEKJOON - 숫자 카드 2

---
+++ BAEKJOON/숫자 카드 2/CodingTest/CodingTest.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+int main()
+{
+	cin.sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int N, M;
+	unordered_map<int, int> uMap;
+	long long input;
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		cin >> input;
+		uMap[input]++;
+	}
+
+	cin >> M;
+	for (int i = 0; i < M; i++) {
+		cin >> input;
+		cout << uMap[input] << ' ';
+	}
+	return 0;
+}
