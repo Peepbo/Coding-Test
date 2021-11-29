e5905023b2879d7559aa5d380870e8ff3100e088
Peepbo:PC <tngus10778@gmail.com>
2021-11-29 22:21:11 +0900

    BAEKJOON - 제로

---
+++ BAEKJOON/제로/CodingTest/CodingTest.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main(void) {
+	stack<int> s;
+	int K, N, sum = 0;
+	cin >> K;
+
+	while (K--) {
+		cin >> N;
+		if (N != 0) {
+			s.push(N);
+		}
+		else {
+			s.pop();
+		}
+	}
+
+	while (!s.empty()) {
+		sum += s.top();
+		s.pop();
+	}
+
+	cout << sum;
+	return 0;
+}
