55ee046a9c3571605cb8fbbca72325b14fc4d3d8
Peepbo:PC <tngus10778@gmail.com>
2021-12-01 20:55:22 +0900

    BAEKJOON - 수 정렬하기 3

---
+++ BAEKJOON/수 정렬하기 3/CodingTest/CodingTest.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int arr[10001];
+
+int main()
+{
+	cin.sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int N, input;
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		cin >> input;
+		arr[input]++;
+	}
+
+	for (int i = 0; i < 10001; i++) {
+		if (arr[i] == 0)continue;
+
+		for (int j = 0; j < arr[i]; j++) {
+			cout << i << '\n';
+		}
+	}
+	return 0;
+}
