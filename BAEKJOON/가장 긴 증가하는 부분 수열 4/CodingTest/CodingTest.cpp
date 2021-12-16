1a7bfb868ba7378a2b82be80d1557899a8244a80
Peepbo:PC <tngus10778@gmail.com>
2021-12-16 23:22:19 +0900

    BAEKJOON - 가장 긴 증가하는 부분 수열 4

---
+++ BAEKJOON/가장 긴 증가하는 부분 수열 4/CodingTest/CodingTest.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int A[1'001], dp[1'001], high[1'001], start;
+
+int main()
+{
+	cin.sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	// Init
+	int N, maximum = 1;
+	cin >> N;
+	start = N;
+
+	for (int i = 1; i <= N; i++) {
+		cin >> A[i];
+		dp[i] = 1;
+	}
+
+	for (int i = N - 1; i >= 1; i--) {
+		for (int j = i + 1; j <= N; j++) {
+			if (A[i] < A[j]) {
+				if (dp[i] < dp[j] + 1) {
+					dp[i] = dp[j] + 1;
+					high[i] = j;
+
+					if (maximum < dp[i]) {
+						maximum = dp[i];
+						start = i;
+					}
+				}
+			}
+		}
+	}
+
+	cout << maximum << '\n';
+	while (high[start] != 0) {
+		cout << A[start] << ' ';
+		start = high[start];
+	}
+	cout << A[start];
+	return 0;
+}
