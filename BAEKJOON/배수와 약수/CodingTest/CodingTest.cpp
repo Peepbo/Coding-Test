b1c2d03e520d6edb09d833f809da75c0f0c50fd5
Peepbo:PC <tngus10778@gmail.com>
2021-12-23 09:55:32 +0900

    BAEKJOON - 배수와 약수

---
+++ BAEKJOON/배수와 약수/CodingTest/CodingTest.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cin.sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int a, b;
+	while (true) {
+		cin >> a >> b;
+		if (a == 0 && b == 0)break;
+
+		if (b % a == 0)cout << "factor\n";
+		else if (a % b == 0)cout << "multiple\n";
+		else cout << "neither\n";
+	}
+}
