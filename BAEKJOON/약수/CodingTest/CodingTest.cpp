a2130d7c1c8b76978d02174bea10c5b2d516ac3b
Peepbo:PC <tngus10778@gmail.com>
2021-12-24 15:57:08 +0900

    BAEKJOON - 약수

---
+++ BAEKJOON/약수/CodingTest/CodingTest.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cin.sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int N, num, low = 1'000'000, high = 2;
+	cin >> N;
+	while (N--) {
+		cin >> num;
+		low = min(low, num);
+		high = max(high, num);
+	}
+	cout << low * high;
+}
