dd9edd70118469ba10992fd46e4af013bc8f1d5d
Peepbo:PC <tngus10778@gmail.com>
2021-12-28 16:40:13 +0900

    BAEKJOON - 이항 계수 1

---
+++ BAEKJOON/이항 계수 1/CodingTest/CodingTest.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int factorial[11];
+
+void initFactorial() {
+	factorial[0] = factorial[1] = 1;
+	for (int i = 2; i <= 10; i++) {
+		factorial[i] = factorial[i - 1] * i;
+	}
+}
+
+int main()
+{
+	cin.sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	initFactorial();
+
+	int N, K, top, down;
+	cin >> N >> K;
+	
+	top = factorial[N];
+	down = factorial[K] * factorial[N - K];
+
+	cout << top / down;
+}
