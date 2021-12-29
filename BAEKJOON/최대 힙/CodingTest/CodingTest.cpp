31de136937e180a05c80795861f22a66a355c550
Peepbo <60508652+Peepbo@users.noreply.github.com>
2021-12-29 23:36:30 +0900

    BAEKJOON - 최대 힙

---
+++ BAEKJOON/최대 힙/CodingTest/CodingTest.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+	cin.sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	priority_queue<int> pq;
+	int N, x;
+	cin >> N;
+	while (N--) {
+		cin >> x;
+		
+		if (x != 0) {
+			pq.push(x);
+		}
+		else {
+			if (!pq.empty()) {
+				cout << pq.top() << '\n';
+				pq.pop();
+			}
+			else {
+				cout << 0 << '\n';
+			}
+		}
+	}
+}
