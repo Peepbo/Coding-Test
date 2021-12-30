3e7d70caa073975864d79ca8407e517c2d57b807
Peepbo <60508652+Peepbo@users.noreply.github.com>
2021-12-30 17:10:01 +0900

    BAEKJOON - 절댓값 힙

---
+++ BAEKJOON/절댓값 힙/CodingTest/CodingTest.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+	cin.sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	priority_queue<pair<int,int>> pq;
+	int N, x;
+	cin >> N;
+	while (N--) {
+		cin >> x;
+		
+		if (x != 0) {
+			pq.push({ -abs(x), -x });
+		}
+		else {
+			if (!pq.empty()) {
+				cout << -pq.top().second << '\n';
+				pq.pop();
+			}
+			else {
+				cout << 0 << '\n';
+			}
+		}
+	}
+}
