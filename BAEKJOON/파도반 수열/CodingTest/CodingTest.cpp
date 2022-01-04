593fb5e71aeeb253ca8d00251eddd32a363afc57
Peepbo <60508652+Peepbo@users.noreply.github.com>
2022-01-04 17:00:02 +0900

    BAEKJOON - 파도반 수열

---
+++ BAEKJOON/파도반 수열/CodingTest/CodingTest.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+long long P[101] { 0,1,1,1,2,2 };
+
+int main(void) {
+	for (int i = 6; i <= 100; i++) {
+		P[i] = P[i - 1] + P[i - 5];
+	}
+
+	int T, N;
+	cin >> T;
+
+	while (T--) {
+		cin >> N;
+		cout << P[N] << '\n';
+	}
+
+	return 0;
+}
