863c53decb45e56a74a8f58b6404fcdf552d965b
Peepbo <60508652+Peepbo@users.noreply.github.com>
2022-01-10 17:00:02 +0900

    BAEKJOON - 상근이의 여행

---
+++ BAEKJOON/상근이의 여행/CodingTest/CodingTest.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> link;
+vector<bool> visited;
+int result;
+
+void bfs(int idx) {
+	visited[idx] = true;
+
+	for (const int& next : link[idx]) {
+		if (visited[next])continue;
+
+		result++;
+		bfs(next);
+	}
+}
+
+int main(void) {
+	cin.sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int T, N, M, a, b;
+	cin >> T;
+	while (T--) {
+		cin >> N >> M;
+		link = vector<vector<int>>(N + 1);
+		visited = vector<bool>(N + 1);
+		result = 0;
+
+		while (M--) {
+			cin >> a >> b;
+			link[a].emplace_back(b);
+			link[b].emplace_back(a);
+		}
+
+		bfs(1);
+		cout << result << '\n';
+	}
+
+    return 0;
+}
