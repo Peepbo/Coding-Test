8fd33e1bf5cd1a5cfb6a9feec6969a9295a47faf
Peepbo <60508652+Peepbo@users.noreply.github.com>
2022-01-17 20:30:43 +0900

    BAEKJOON - 패션왕 신해빈

---
+++ BAEKJOON/패션왕 신해빈/CodingTest/CodingTest.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <map>
+
+using namespace std;
+
+map<string, int> m;
+
+int calculus(int f) {
+	if (f == 0)return 0;
+	return (f * (f + 1)) / 2 + calculus(f - 1);
+}
+
+int main()
+{
+	cin.sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int T, n, way;
+	string name, kind;
+	cin >> T;
+	while (T--) {
+		cin >> n;
+		way = 1;
+		m.clear();
+
+		while (n--) {
+			cin >> name >> kind;
+			m[kind]++;
+		}
+
+		for (const auto& pr : m) {
+			way *= pr.second + 1;
+		}
+		cout << way - 1 << '\n';
+	}
+}
