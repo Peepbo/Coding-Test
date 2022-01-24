f8860bb519bc79219a487158dde10dfc55e98c35
Peepbo <60508652+Peepbo@users.noreply.github.com>
2022-01-24 09:52:45 +0900

    BAEKJOON - 집합의 표현

---
+++ BAEKJOON/집합의 표현/CodingTest/CodingTest.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int Parent[1'000'001];
+
+int GetParent(int a) {
+	if (a == Parent[a]) return a;
+	return Parent[a] = GetParent(Parent[a]);
+}
+
+bool IsSameParent(int a, int b) {
+	a = GetParent(a);
+	b = GetParent(b);
+	return a == b;
+}
+
+void UnionParent(int a, int b) {
+	a = GetParent(a);
+	b = GetParent(b);
+	a < b ? Parent[b] = a : Parent[a] = b;
+}
+
+void Kruskal() {
+	int n, m, a, b;
+	bool isIntersection;
+
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) {
+		Parent[i] = i;
+	}
+
+	while (m--) {
+		cin >> isIntersection >> a >> b;
+
+		if (isIntersection) {
+			IsSameParent(a, b) ? cout << "YES\n" : cout << "NO\n";
+		}
+		else {
+			if (IsSameParent(a, b) == false) {
+				UnionParent(a, b);
+			}
+		}
+	}
+}
+
+int main(void) {
+	cin.sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	
+	Kruskal();
+
+	return 0;
+}
