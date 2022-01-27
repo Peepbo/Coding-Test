8a7f359f665434bec8eba91f275ee5162a06e097
Peepbo <60508652+Peepbo@users.noreply.github.com>
2022-01-27 12:59:27 +0900

    BAEKJOON - 피보나치 함수

---
+++ BAEKJOON/피보나치 함수/CodingTest/CodingTest.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+pair<int, int> fibonacci[41];
+
+const pair<int, int> sum(const pair<int, int>& p1, const pair<int, int>& p2) {
+	return { p1.first + p2.first, p1.second + p2.second };
+}
+
+int main(void) {
+	fibonacci[0] = { 1,0 };
+	fibonacci[1] = { 0,1 };
+	for (int i = 2; i <= 40; i++) {
+		fibonacci[i] = sum(fibonacci[i - 1], fibonacci[i - 2]);
+	}
+
+	int T, N;
+	cin >> T;
+	while (T--) {
+		cin >> N;
+		printf("%d %d\n", fibonacci[N].first, fibonacci[N].second);
+	}
+
+	return 0;
+}
