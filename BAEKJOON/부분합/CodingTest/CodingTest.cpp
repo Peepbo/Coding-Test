7a666a3450db3979e10882b60cf07e6188ce7edd
Peepbo <60508652+Peepbo@users.noreply.github.com>
2022-01-31 13:28:22 +0900

    BAEKJOON - 부분합

---
+++ BAEKJOON/부분합/CodingTest/CodingTest.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+#define INF 100'001
+int arr[INF];
+
+int main(void) {
+	cin.sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	
+	int N, S;
+	int s, sum, len = INF;
+	cin >> N >> S;
+
+	for (int i = 1; i <= N; i++) {
+		cin >> arr[i];
+	}
+
+	s = 1;
+	sum = 0;
+	
+	for (int e = 1; e <= N; e++) {
+		sum += arr[e];
+
+		if (sum < S)continue;
+
+		while (sum - arr[s] >= S) {
+			sum -= arr[s++];
+		}
+
+		len = min(len, e - s + 1);
+	}
+
+	cout << (len != INF ? len : 0);
+	return 0;
+}
