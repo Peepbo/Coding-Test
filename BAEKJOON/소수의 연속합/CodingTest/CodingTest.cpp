2cd93eb1ab526115e5e78bab978b395ce512c8f3
Peepbo <60508652+Peepbo@users.noreply.github.com>
2022-02-02 18:53:37 +0900

    BAEKJOON - 소수의 연속합

---
+++ BAEKJOON/소수의 연속합/CodingTest/CodingTest.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N;
+bool isDecimal[4'000'001];
+vector<int> decimalNum;
+
+void Eratos() {
+	fill(isDecimal, isDecimal + 4'000'000, true);
+
+	const int halfN = N / 2;
+
+	for (int i = 2; i <= halfN; i++) {
+		if (!isDecimal[i]) continue;
+
+		for (int j = i + i; j <= N; j += i) {
+			isDecimal[j] = false;
+		}
+	}
+}
+
+void Init() {
+	for (int i = 2; i <= N; i++) {
+		if (isDecimal[i]) {
+			decimalNum.emplace_back(i);
+		}
+	}
+}
+
+int Solve() {
+	const int rimit = decimalNum.size();
+	int count = 0;
+	int s = 0, e = 0;
+	int sum = decimalNum.empty() ? 0 : decimalNum.front();
+
+	if (rimit == 0)return rimit;
+
+	while (s <= e) {
+		if (sum == N) {
+			count++;
+		}
+
+		if (sum < N) {
+			e++;
+			if (e == rimit)break;
+			sum += decimalNum[e];
+		}
+		else {
+			sum -= decimalNum[s];
+			s++;
+		}
+	}
+
+	return count;
+}
+
+int main(void) {
+	cin.sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> N;
+
+	Eratos();
+	Init();
+
+	cout << Solve();
+	return 0;
+}
