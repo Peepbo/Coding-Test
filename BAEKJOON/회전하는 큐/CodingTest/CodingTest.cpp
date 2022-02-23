98072e305fc33984a3f340d32103dedce4132274
CHAE <tngus10778@gmail.com>
2022-02-23 23:54:43 +0900

    BAEKJOON - 회전하는 큐

---
+++ BAEKJOON/회전하는 큐/CodingTest/CodingTest.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <deque>
+
+using namespace std;
+
+int main(void) {
+	deque<int> dq, leftDp, rightDp;
+	int N, M, num, cost = 0, leftMove, rightMove;
+	cin >> N >> M;
+
+	for (int i = 1; i <= N; i++) {
+		dq.push_back(i);
+	}
+
+	while (M--) {
+		cin >> num;
+
+		if (dq.front() == num) {
+			dq.pop_front();
+		}
+
+		else {
+			leftDp = rightDp = dq;
+			leftMove = rightMove = 0;
+
+			while (leftDp.front() != num) {
+				leftDp.push_back(leftDp.front());
+				leftDp.pop_front();
+				leftMove++;
+			}
+
+			while (rightDp.front() != num) {
+				rightDp.push_front(rightDp.back());
+				rightDp.pop_back();
+				rightMove++;
+			}
+
+			cost += min(leftMove, rightMove);
+			dq = leftDp;
+			dq.pop_front();
+		}
+	}
+
+	cout << cost;
+	return 0;
+}
