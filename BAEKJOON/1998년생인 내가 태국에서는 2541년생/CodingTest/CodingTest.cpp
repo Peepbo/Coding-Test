c86a107c62a76054c135d2e612b330a14994e5cc
CHAE <tngus10778@gmail.com>
2022-03-16 20:52:41 +0900

    BAEKJOON - 1998년생인 내가 태국에서는 2541년생

---
+++ BAEKJOON/1998년생인 내가 태국에서는 2541년생/CodingTest/CodingTest.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	cin.sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int y;
+	cin >> y;
+
+	cout << (y - 543);
+	return 0;
+}
