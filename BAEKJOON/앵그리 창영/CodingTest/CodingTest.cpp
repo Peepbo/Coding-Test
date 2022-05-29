5193968d2d29f1e444edb57064e08c5ebf3ace68
CHAE <tngus10778@gmail.com>
2022-05-29 22:49:20 +0900

    BAEKJOON - 앵그리 창영

---
+++ BAEKJOON/앵그리 창영/CodingTest/CodingTest.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N, W, H;
+	float T;
+	cin >> N >> W >> H;
+
+	const float Diagonal = sqrt(W * W + H * H);
+	while (N--)
+	{
+		cin >> T;
+		if (T <= Diagonal)
+		{
+			cout << "DA" << '\n';
+		}
+		else
+		{
+			cout << "NE" << '\n';
+		}
+	}
+
+	return 0;
+}
