648a983fcad966bd961ca8c6c67acd037a07b100
CHAE <tngus10778@gmail.com>
2022-06-01 17:36:46 +0900

    BAEKJOON - 오븐 시계

---
+++ BAEKJOON/오븐 시계/CodingTest/CodingTest.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int A, B, C, Hour, Minute;
+	cin >> A >> B >> C;
+	Hour = A;
+	Minute = B + C;
+
+	if (Minute >= 60)
+	{
+		Hour += Minute / 60;
+		Minute %= 60;
+	}
+	if (Hour >= 24)
+	{
+		Hour %= 24;
+	}
+
+	cout << Hour << ' ' << Minute;
+	return 0;
+}
