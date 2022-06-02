2cff364a06305ff094b2ebf34d6f991d6c30f752
CHAE <tngus10778@gmail.com>
2022-06-02 16:09:40 +0900

    BAEKJOON - 단어 정렬

---
+++ BAEKJOON/단어 정렬/CodingTest/CodingTest.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+
+bool compare(const string& S1, const string& S2)
+{
+	if (S1.length() != S2.length())
+	{
+		return S1.length() < S2.length();
+	}
+	return S1 < S2;
+}
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N;
+	string Input;
+	cin >> N;
+	vector<string> Arr;
+	set<string> WordSet;
+	while (N--)
+	{
+		cin >> Input;
+		if (WordSet.find(Input) == WordSet.end())
+		{
+			WordSet.insert(Input);
+			Arr.emplace_back(Input);
+		}
+	}
+	sort(Arr.begin(), Arr.end(), compare);
+	for (const string& S : Arr)
+	{
+		cout << S << '\n';
+	}
+	return 0;
+}
