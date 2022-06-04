eec96c15833426c547962a06e3124194b5f9803d
CHAE <tngus10778@gmail.com>
2022-06-04 17:10:31 +0900

    BAEKJOON - 대칭 차집합

---
+++ BAEKJOON/대칭 차집합/CodingTest/CodingTest.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	unordered_set<int> ElementList;
+	int N, M;
+	int Input;
+	cin >> N >> M;
+	while (N--)
+	{
+		cin >> Input;
+		ElementList.insert(Input);
+	}
+	while (M--)
+	{
+		cin >> Input;
+		const bool bFound{ ElementList.find(Input) != ElementList.end() };
+
+		if (bFound)
+		{
+			ElementList.erase(Input);
+		}
+		else
+		{
+			ElementList.insert(Input);
+		}
+	}
+
+	cout << ElementList.size();
+	return 0;
+}
