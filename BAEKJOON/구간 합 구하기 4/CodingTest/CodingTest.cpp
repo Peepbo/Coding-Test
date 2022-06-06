63f67010cd065b2757a85aa2d282d3929d4a508d
CHAE <tngus10778@gmail.com>
2022-06-06 12:09:57 +0900

    BAEKJOON - 구간 합 구하기 4

---
+++ BAEKJOON/구간 합 구하기 4/CodingTest/CodingTest.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N, M;
+	int Input;
+	cin >> N >> M;
+	vector<int> Arr(N), SumList(N);
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> Input;
+		Arr[i] = Input;
+
+		SumList[i] = Input;
+		if (i > 0)
+		{
+			SumList[i] += SumList[i - 1];
+		}
+	}
+
+	int i, j;
+	while (M--)
+	{
+		cin >> i >> j;
+		i--; j--;
+
+		int Sum = SumList[j];
+		if (i > 0)
+		{
+			Sum -= SumList[i - 1];
+		}
+		cout << Sum << '\n';
+	}
+
+	return 0;
+}
