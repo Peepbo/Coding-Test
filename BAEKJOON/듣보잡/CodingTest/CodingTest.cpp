16a76a7ce3886873d0d7052dbbc94e30caa019c3
CHAE <tngus10778@gmail.com>
2022-06-08 13:48:30 +0900

    BAEKJOON - 듣보잡

---
+++ BAEKJOON/듣보잡/CodingTest/CodingTest.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	unordered_map<string, int> UnknownList;
+	vector<string> OutputList;
+	int N, M;
+	string Input;
+
+	cin >> N >> M;
+	while (N--)
+	{
+		cin >> Input;
+		UnknownList[Input]++;
+	}
+	while (M--)
+	{
+		cin >> Input;
+		UnknownList[Input]++;
+	}
+
+	for (const auto& PairElement : UnknownList)
+	{
+		if (PairElement.second == 2)
+		{
+			OutputList.emplace_back(PairElement.first);
+		}
+	}
+
+	sort(OutputList.begin(), OutputList.end());
+	cout << OutputList.size() << '\n';
+	for (const string& Name : OutputList)
+	{
+		cout << Name << '\n';
+	}
+	return 0;
+}
