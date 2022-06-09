3c248b45ad8bd73c2d3c9719492135086a0486c3
CHAE <tngus10778@gmail.com>
2022-06-09 11:18:25 +0900

    BAEKJOON - 문자열 집합

---
+++ BAEKJOON/문자열 집합/CodingTest/CodingTest.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	unordered_set<string> NumberSet;
+	int N, M, Count = 0;
+	string Input;
+	cin >> N >> M;
+	while (N--)
+	{
+		cin >> Input;
+		NumberSet.insert(Input);
+	}
+	while (M--)
+	{
+		cin >> Input;
+
+		if (NumberSet.find(Input) != NumberSet.end())
+		{
+			Count++;
+		}
+	}
+
+	cout << Count;
+	return 0;
+}
