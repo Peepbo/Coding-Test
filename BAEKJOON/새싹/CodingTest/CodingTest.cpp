babcc0cb32af0964e91c756e663922128b171c80
CHAE <tngus10778@gmail.com>
2022-06-13 18:20:33 +0900

    BAEKJOON - 새싹

---
+++ BAEKJOON/새싹/CodingTest/CodingTest.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cout << "         ,r'\"7" << '\n'
+		 << "r`-_   ,'  ,/" << '\n'
+		 << " \\. \". L_r'" << '\n'
+		 << "   `~\\/" << '\n'
+		 << "      |" << '\n'
+		 << "      |";
+	return 0;
+}
