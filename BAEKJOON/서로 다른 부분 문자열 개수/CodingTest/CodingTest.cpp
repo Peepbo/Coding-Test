78171bea517adafd47fbf7531a53d8c6f3d70f11
CHAE <tngus10778@gmail.com>
2022-06-14 21:54:47 +0900

    BAEKJOON - 서로 다른 부분 문자열 개수

---
+++ BAEKJOON/서로 다른 부분 문자열 개수/CodingTest/CodingTest.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	unordered_set<string> WordList;
+	string Word, SubWord;
+	cin >> Word;
+
+	/* s : 시작, e : 끝 */
+	for (int s = 0; s < Word.length(); s++)
+	{
+		for (int e = s; e < Word.length(); e++)
+		{
+			SubWord = Word.substr(s, e - s + 1);
+
+			WordList.insert(SubWord);
+		}
+	}
+
+	cout << WordList.size();
+	return 0;
+}
