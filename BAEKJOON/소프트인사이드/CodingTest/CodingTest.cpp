f5518ace1976c35ea83ef85f258c3419d90fbbad
CHAE <tngus10778@gmail.com>
2022-06-15 17:21:21 +0900

    BAEKJOON - 소프트인사이드

---
+++ BAEKJOON/소프트인사이드/CodingTest/CodingTest.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	string N;
+	cin >> N;
+	sort(N.begin(), N.end(), greater<>());
+
+	cout << N;
+	return 0;
+}
