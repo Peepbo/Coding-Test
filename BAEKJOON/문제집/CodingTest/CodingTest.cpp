da4e54ded63ebf0b1c814a786e48b30c7a9953dc
CHAE <tngus10778@gmail.com>
2022-06-16 20:58:24 +0900

    BAEKJOON - 문제집

---
+++ BAEKJOON/문제집/CodingTest/CodingTest.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+vector<vector<int>> Link;
+int InDegree[32'001];
+
+void TopologicalSort(int N)
+{
+	priority_queue<int, vector<int>, greater<int>> SolutionQueue;
+
+	for (int i = 1; i <= N; i++)
+	{
+		if (InDegree[i] == 0)
+		{
+			SolutionQueue.push(i);
+		}
+	}
+
+	int Now;
+	while (!SolutionQueue.empty())
+	{
+		Now = SolutionQueue.top();
+		SolutionQueue.pop();
+
+		cout << Now << ' ';
+		for (int Next : Link[Now])
+		{
+			if (--InDegree[Next] == 0)
+			{
+				SolutionQueue.push(Next);
+			}
+		}
+	}
+}
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	int N, M;
+	cin >> N;
+	Link.assign(N + 1, {});
+	int A, B;
+
+	cin >> M;
+	while (M--)
+	{
+		cin >> A >> B;
+		Link[A].emplace_back(B);
+		InDegree[B]++;
+	}
+
+	TopologicalSort(N);
+	return 0;
+}
