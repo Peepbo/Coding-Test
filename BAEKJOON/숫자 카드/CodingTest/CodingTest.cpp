707805c659612ff18f131983dc555e7d610179d1
CHAE <tngus10778@gmail.com>
2022-06-19 15:15:54 +0900

    BAEKJOON - 숫자 카드

---
+++ BAEKJOON/숫자 카드/CodingTest/CodingTest.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	unordered_set<int> NumberSet;
+	int N, M, Input;
+	cin >> N;
+	while (N--)
+	{
+		cin >> Input;
+		NumberSet.insert(Input);
+	}
+
+	cin >> M;
+	while (M--)
+	{
+		cin >> Input;
+
+		cout << (NumberSet.find(Input) != NumberSet.end() ? 1 : 0) << ' ';
+	}
+
+	return 0;
+}
