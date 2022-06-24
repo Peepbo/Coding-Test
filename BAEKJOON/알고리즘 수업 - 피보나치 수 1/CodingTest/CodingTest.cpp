68636b1da84c570a4b2f99cbe97c4722b3cacd47
CHAE <tngus10778@gmail.com>
2022-06-24 15:47:22 +0900

    BAEKJOON - 알고리즘 수업 - 피보나치 수 1

---
+++ BAEKJOON/알고리즘 수업 - 피보나치 수 1/CodingTest/CodingTest.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int Fib1CallCount, Fib2CallCount;
+
+int fib(int n)
+{
+	if (n == 1 || n == 2)
+	{
+		Fib1CallCount++;
+
+		return 1;
+	}
+	return fib(n - 1) + fib(n - 2);
+}
+
+int F[2]{ 1,1 };
+
+int fibonacci(int n)
+{
+	for (int i = 3; i <= n; i++)
+	{
+		Fib2CallCount++;
+
+		int Temp = F[1];
+		F[1] = F[0] + F[1];
+		F[0] = Temp;
+	}
+
+	return F[1];
+}
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	fib(n);
+	fibonacci(n);
+	
+	cout << Fib1CallCount << ' ' << Fib2CallCount;
+	return 0;
+}
