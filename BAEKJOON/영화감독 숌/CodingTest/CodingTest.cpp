e59ee4b2045314bd6866753a01175dce523c9c33
CHAE <tngus10778@gmail.com>
2022-06-25 00:26:27 +0900

    BAEKJOON - 영화감독 숌

---
+++ BAEKJOON/영화감독 숌/CodingTest/CodingTest.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+bool CheckNumber(int Number)
+{
+	const string NumberStr = to_string(Number);
+
+	for (int i = 0; i < NumberStr.length() - 2; i++)
+	{
+		if (NumberStr[i] == '6' && NumberStr[i + 1] == '6' && NumberStr[i + 2] == '6')
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N;
+	cin >> N;
+
+	int Number = 665;
+	int Count = 0;
+
+	while (true)
+	{
+		if (CheckNumber(Number))
+		{
+			Count++;
+
+			if (N == Count)
+			{
+				break;
+			}
+		}
+
+		Number++;
+	}
+
+	cout << Number;
+	return 0;
+}
