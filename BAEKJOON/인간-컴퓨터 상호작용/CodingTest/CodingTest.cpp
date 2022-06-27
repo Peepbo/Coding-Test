829acc1db0c9b93ed9e26510a33afb60abf2d2e2
CHAE <tngus10778@gmail.com>
2022-06-27 15:45:25 +0900

    BAEKJOON - 인간-컴퓨터 상호작용

---
+++ BAEKJOON/인간-컴퓨터 상호작용/CodingTest/CodingTest.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	string S;
+	int q;
+	char Alphabet;
+	int Start, End;
+	cin >> S >> q;
+
+	const int Length = S.length();
+	map<char, vector<int>> SumMap;
+
+	while (q--)
+	{
+		cin >> Alphabet >> Start >> End;
+
+		if (SumMap.find(Alphabet) == SumMap.end())
+		{
+			SumMap[Alphabet].assign(Length, 0);
+
+			for (int i = 0; i < Length; i++)
+			{
+				if (S[i] == Alphabet)
+				{
+					SumMap[Alphabet][i]++;
+				}
+
+				if (i > 0)
+				{
+					SumMap[Alphabet][i] += SumMap[Alphabet][i - 1];
+				}
+			}
+		}
+
+		const int Sub{ Start == 0 ? 0 : SumMap[Alphabet][Start - 1] };
+		cout << SumMap[Alphabet][End] - Sub << '\n';
+	}
+
+	return 0;
+}
