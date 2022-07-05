4242197bf4c45c47eb3bc50fd0a2671f33eb3007
CHAE <tngus10778@gmail.com>
2022-07-05 21:15:06 +0900

    BAEKJOON - 좌표 정렬하기

---
+++ BAEKJOON/좌표 정렬하기/CodingTest/CodingTest.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N, X, Y;
+	cin >> N;
+	vector<pair<int,int>> Points(N);
+	while (N--)
+	{
+		cin >> X >> Y;
+		Points[N] = { X, Y };
+	}
+	sort(Points.begin(), Points.end());
+	for (const pair<int, int>& P : Points)
+	{
+		cout << P.first << ' ' << P.second << '\n';
+	}
+	return 0;
+}
