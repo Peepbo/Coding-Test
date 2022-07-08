6761bcdc5f85d1439ede595852b2b99bf6fd3736
CHAE <tngus10778@gmail.com>
2022-07-08 22:17:11 +0900

    BAEKJOON - 줄 세우기

---
+++ BAEKJOON/줄 세우기/CodingTest/CodingTest.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int InDegree[32'001];
+vector<vector<int>> Link;
+
+void TopologySort(int N)
+{
+	queue<int> SolutionQueue;
+	for (int i = 1; i <= N; i++)
+	{
+		if (InDegree[i] == 0)
+		{
+			SolutionQueue.push(i);
+		}
+	}
+
+	while (!SolutionQueue.empty())
+	{
+		int Select = SolutionQueue.front();
+		SolutionQueue.pop();
+
+		cout << Select << ' ';
+		for (int Next : Link[Select])
+		{
+			if (--InDegree[Next] == 0)
+			{
+				SolutionQueue.push(Next);
+			}
+		}
+	}
+}
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N, M;
+	cin >> N >> M;
+
+	int A, B;
+	Link.assign(N + 1, {});
+	
+	while (M--)
+	{
+		cin >> A >> B;
+		Link[A].emplace_back(B);
+		InDegree[B]++;
+	}
+
+	TopologySort(N);
+
+	return 0;
+}
