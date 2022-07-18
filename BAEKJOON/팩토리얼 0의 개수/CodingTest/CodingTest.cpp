cb830c5fc2eff8f36eeb848bc1c28ae641db450d
CHAE <tngus10778@gmail.com>
2022-07-18 21:26:56 +0900

    BAEKJOON - 팩토리얼 0의 개수

---
+++ BAEKJOON/팩토리얼 0의 개수/CodingTest/CodingTest.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N;
+	cin >> N;
+
+	cout << N / 5 + N / 25 + N / 125;
+	return 0;
+}
